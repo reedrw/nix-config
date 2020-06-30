7904d41564ec6870428c37abea38d46bcdc36d56
Reed <mrnitsuj1@gmail.com>
2020-06-30 04:58:32 -0400

    actually fix install script

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash -p jq
+#! /usr/bin/env bash
 
-nix-channel --add "$(jq -r '.["home-manager"].url' ./nix-home/nix/sources.json )" home-manager
+nix-channel --add "$(nix run nixpkgs.jq -c jq -r '.["home-manager"].url' ./nix-home/nix/sources.json )" home-manager
 nix-channel --update
-home-manager switch
-
+nix-shell --run "home-manager switch"
