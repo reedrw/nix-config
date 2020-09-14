3fcdc86a2d4db2c5f989f096007f9fdfc3d2aa39
Reed <mrnitsuj1@gmail.com>
2020-09-14 05:45:19 -0400

    oops that doesn't  exist lol

--- overlay.nix
+++ overlay.nix
@@ -1,5 +1,4 @@
 self: super: rec {
-  NoiseTorch = super.callPackage ./pkgs/NoiseTorch {};
   artwiz-lemon = super.callPackage ./pkgs/artwiz-lemon {};
   c = super.callPackage ./pkgs/c {};
   comma = super.callPackage ./pkgs/comma {};
