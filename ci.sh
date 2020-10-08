9e3371e1a9087a6a56ed4ca11a2ad1c19357e6ec
Reed <mrnitsuj1@gmail.com>
2020-10-08 07:50:58 -0400

    why do you not work

--- ci.sh
+++ ci.sh
@@ -4,18 +4,20 @@
 case $1 in
   system)
 
-    nix-build '<nixpkgs/nixos>' -A system -I nixos-config=./system/$machine/configuration.nix \
+    nix-build \
+      -I nixos-config=./system/$machine/configuration.nix \
       -I nixos-hardware="$(jq -r '.["nixos-hardware"].url' ./nix/sources.json)" \
       -I nixpkgs="$(jq -r '.["nixpkgs-unstable"].url' ./nix/sources.json)" \
       -I nur="$(jq -r '.["NUR"].url' ./nix/sources.json)" \
-      --no-out-link || \
+      '<nixpkgs/nixos>' -A system --no-out-link || \
       exit 1
   ;;
   home-manager)
 
-    nix-build '<home-manager/home-manager/home-manager.nix>' \
+    nix-build \
       -I home-manager="$(jq -r '.["home-manager"].url' ./nix/sources.json)" \
       -I nixpkgs="$(jq -r '.["nixpkgs-unstable"].url' ./nix/sources.json)" \
+      '<home-manager/home-manager/home-manager.nix>' \
       --attr activationPackage \
       --argstr confPath "$(readlink -f ./home.nix)" \
       --no-out-link || \
