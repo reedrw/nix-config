9e553ae5e4d4abd5edee4cf6b03f49096bd0695c
Reed <mrnitsuj1@gmail.com>
2020-10-08 06:33:28 -0400

    please work this time

--- ci.sh
+++ ci.sh
@@ -6,14 +6,16 @@ case $1 in
     nix-build '<nixpkgs/nixos>' -A system -I nixos-config=./system/$machine/configuration.nix \
       -I nixos-hardware="$(jq -r '.["nixos-hardware"].url' ./nix/sources.json)" \
       -I nixos="$(jq -r '.["nixos-unstable"].url' ./nix/sources.json)" \
-      -I nur="$(jq -r '.["NUR"].url' ./nix/sources.json)" || \
+      -I nur="$(jq -r '.["NUR"].url' ./nix/sources.json)" \
+      --no-out-link || \
       exit 1
   ;;
   home-manager)
     nix-build "<home-manager/home-manager/home-manager.nix>" \
       -I home-manager="$(jq -r '.["home-manager"].url' ./nix/sources.json)" \
       --attr activationPackage \
-      --argstr confPath ./home.nix || \
+      --argstr confPath "$(readlink -f ./home.nix)" \
+      --no-out-link || \
       exit 1
   ;;
 esac
