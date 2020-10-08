70235fd6b6cd4fd7ca135a5965a3bb4e818a7b0c
Reed <mrnitsuj1@gmail.com>
2020-10-08 06:48:52 -0400

    fix typo

--- ci.sh
+++ ci.sh
@@ -3,15 +3,21 @@
 
 case $1 in
   system)
+
+    echo "$ nix-build '<nixpkgs/nixos>' -A system -I nixos-config=./system/$machine/configuration.nix -I nixos-hardware="$(jq -r '.["nixos-hardware"].url' ./nix/sources.json)" -I nixos="$(jq -r '.["nixpkgs-unstable"].url' ./nix/sources.json)" -I nur="$(jq -r '.["NUR"].url' ./nix/sources.json)" --no-out-link"
+
     nix-build '<nixpkgs/nixos>' -A system -I nixos-config=./system/$machine/configuration.nix \
       -I nixos-hardware="$(jq -r '.["nixos-hardware"].url' ./nix/sources.json)" \
-      -I nixos="$(jq -r '.["nixos-unstable"].url' ./nix/sources.json)" \
+      -I nixos="$(jq -r '.["nixpkgs-unstable"].url' ./nix/sources.json)" \
       -I nur="$(jq -r '.["NUR"].url' ./nix/sources.json)" \
       --no-out-link || \
       exit 1
   ;;
   home-manager)
-    nix-build "<home-manager/home-manager/home-manager.nix>" \
+
+    echo "$ nix-build '<home-manager/home-manager/home-manager.nix> -I home-manager="$(jq -r '.["home-manager"].url' ./nix/sources.json)" --attr activationPackage --argstr confPath "$(readlink -f ./home.nix)" --no-out-link"
+
+    nix-build '<home-manager/home-manager/home-manager.nix>' \
       -I home-manager="$(jq -r '.["home-manager"].url' ./nix/sources.json)" \
       --attr activationPackage \
       --argstr confPath "$(readlink -f ./home.nix)" \
