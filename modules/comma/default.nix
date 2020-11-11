30ec516dbf06bc87472f8f1a5dc6a6ba08cabec2
Reed <mrnitsuj1@gmail.com>
2020-11-11 04:18:49 -0500

    stop comma from failing on old commits
    Former-commit-id: b17d801454e04b802e301f09148483305284e61c

--- modules/comma/default.nix
+++ modules/comma/default.nix
@@ -1,9 +1,18 @@
 { config, lib, pkgs, ... }:
 
+let
+
+  indexCache = builtins.fetchurl {
+    url = "https://s3.amazonaws.com/burkelibbey/nix-index-files";
+    sha256 = "06p58f82wipd0a8wbc7j3l0p8iaxvdibgshmc9dbxkjf0hmln3kx";
+  };
+
+in
 {
 
   home.packages = [ pkgs.nur.repos.reedrw.comma ];
 
-  home.file.".cache/nix-index/files".source = ./files;
+  home.file.".cache/nix-index/files".source = if builtins.pathExists ./files
+    then ./files else indexCache;
 
 }
