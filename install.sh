69903651d7570595a13f0c9e1b3332f1053c5014
Reed <mrnitsuj1@gmail.com>
2020-11-12 03:53:23 -0500

    rofi as SUDO_ASKPASS
    Former-commit-id: fcdee9fe4b07d0c8aef431d70b25b56b1f182a2a

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash
 
+if [[ -n "$SUDO_ASKPASS" ]]; then
+  sudo="sudo -A"
+fi
+
 HomeManagerURL="$(jq -r '.["home-manager"].url' ./nix/sources.json)"
 nixpkgsURL="$(jq -r '.["nixpkgs-unstable"].url' ./nix/sources.json)"
 nurURL="$(jq -r '.["NUR"].url' ./nix/sources.json)"
@@ -13,21 +17,21 @@ if [[ "$installedHomeManager" != "$HomeManagerURL" ]]; then
   nix-channel --update
 fi
 
-installedNixpkgs="$(sudo nix-channel --list | grep "nixos " | cut -d' ' -f2-)"
+installedNixpkgs="$($sudo nix-channel --list | grep "nixos " | cut -d' ' -f2-)"
 if [[ "$installedNixpkgs" != "$nixpkgsURL" ]]; then
   echo "Installing pinned nixpkgs..."
   sudo nix-channel --add "$nixpkgsURL" nixos
   sudo nix-channel --update
 fi
 
-installedNixOShardware="$(sudo nix-channel --list | grep "nixos-hardware " | cut -d' ' -f2-)"
+installedNixOShardware="$($sudo nix-channel --list | grep "nixos-hardware " | cut -d' ' -f2-)"
 if [[ "$installedNixOShardware" != "$NixOShardwareURL" ]]; then
   echo "Installing pinned nixos-hardware..."
   sudo nix-channel --add "$NixOShardwareURL" nixos-hardware
   sudo nix-channel --update
 fi
 
-installedNUR="$(sudo nix-channel --list | grep "nur " | cut -d' ' -f2-)"
+installedNUR="$($sudo nix-channel --list | grep "nur " | cut -d' ' -f2-)"
 if [[ "$installedNUR" != "$nurURL" ]]; then
   echo "Installing pinned NUR..."
   sudo nix-channel --add "$nurURL" nur
--- modules/rofi/default.nix
+++ modules/rofi/default.nix
@@ -153,5 +153,50 @@
       }
     '';
   };
+
+  programs.zsh.initExtra = let
+    askpass-rasi = with config.lib.base16.theme; builtins.toFile "askpass.rasi" ''
+      * {
+          background-color: #${base00-hex};
+          text-color:       #${base07-hex};
+      }
+
+      #window {
+          /* Change the this according to your screen width */
+          width:      380px;
+
+          /* Change this value according to your screen height */
+          y-offset: -5%;
+
+          /* This padding is given just for aesthetic purposes */
+          padding:    40px;
+      }
+
+
+      #entry {
+          /*
+           * For some reason, without this option, a dash/hyphen appears
+           * at the end of the entry
+           */
+          expand: true;
+
+          /* Keeping using 200px so that long passwords can be typed */
+          width: 200px;
+      }
+    '';
+    rofi-askpass = pkgs.writeShellScript "rofi-askpass" ''
+      rofi -dmenu\
+        -password\
+        -i\
+        -no-fixed-num-lines\
+        -p "Password: "\
+        -theme ${askpass-rasi} \
+        2> /dev/null
+    '';
+  in ''
+    export SUDO_ASKPASS=${rofi-askpass}
+    alias sudo="sudo -A"
+  '';
+
 }
 
