672a17628c48f39e6788c98a77dfa1a51e34c706
Reed <mrnitsuj1@gmail.com>
2020-11-14 01:51:07 -0500

    add system and home-manager attributes
    Former-commit-id: 0c0d8d6510834dcc516b4f1045a294fec083e40b

--- ci.nix
+++ ci.nix
@@ -3,12 +3,14 @@ let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs-unstable;
 
-
 in
 {
 
-  t400 = (import "${sources.nixpkgs-unstable}/nixos" { configuration = import ./system/nixos-t400/configuration.nix; }).system;
-  t520 = (import "${sources.nixpkgs-unstable}/nixos" { configuration = import ./system/nixos-t520/configuration.nix; }).system;
-  desktop = (import "${sources.nixpkgs-unstable}/nixos" { configuration = import ./system/nixos-desktop/configuration.nix; }).system;
+  nixos-t400 = (import "${sources.nixpkgs-unstable}/nixos" { configuration = import ./system/nixos-t400/configuration.nix; }).system;
+  nixos-t520 = (import "${sources.nixpkgs-unstable}/nixos" { configuration = import ./system/nixos-t520/configuration.nix; }).system;
+  nixos-desktop = (import "${sources.nixpkgs-unstable}/nixos" { configuration = import ./system/nixos-desktop/configuration.nix; }).system;
+
+  home-manager = (import "${sources.home-manager}/home-manager/home-manager.nix" { pkgs = import "${sources.nixpkgs-unstable}" {}; confPath = ./home.nix; } ).activationPackage;
+
 
 }
