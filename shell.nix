75ba4a14981cd8c76e5a31993c84adbfd96c7b8a
Reed <mrnitsuj1@gmail.com>
2021-01-08 05:44:53 -0500

    fix shell.nix
    Former-commit-id: b7c637235dbbf52044bfb81f49192ccd528b7a47

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@ with import <nixpkgs> { };
 let
   sources = import ./nix/sources.nix;
 
-  devshell = import "${sources.devshell}/nix/overlay.nix";
+  devshell = import "${sources.devshell}/overlay.nix";
 
   hm-overlay = self: super: {
     home-manager = super.callPackage "${sources.home-manager}/home-manager" { };
