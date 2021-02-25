602e5589c3a2dc8cf6dee55fe96311ee269454fc
Reed <mrnitsuj1@gmail.com>
2021-02-24 20:30:16 -0500

    create pull-and-install script

---
+++ pull-and-install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash -p jq gh git curl
+
+set -e
+set -x
+
+api="https://api.github.com"
+user="reedrw"
+repo="nix-config"
+
+mainUrl="$api/repos/$user/$repo"
+
+# Get latest reedbot PR number
+prNumber="$(curl -s "$mainUrl/pulls" | jq -r '.[] | select(.user.login == "reedbot[bot]") | .number')"
+
+# Get PR ref
+prRef="$(curl -s "$mainUrl/pulls/$prNumber" | jq -r '.head.sha')"
+
+# Get GitHub Action workflow conclusion
+prConclusion="$(curl -s "$mainUrl/commits/$prRef/check-suites" | jq -r '.check_suites[] | select(.app.slug == "github-actions") | .conclusion')"
+
+main(){
+  if [[ $prConclusion == "success" ]]; then
+    pushd ~/.config/nixpkgs || exit
+    gh pr merge "$prNumber" -dm
+    git pull
+    ./install.sh
+    popd || exit
+  else
+    echo 'GitHub Actions conclusion is not "success"'
+    exit 1
+  fi
+}
+
+main "$@"
