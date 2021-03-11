29afeb2611fd377546251a1d3afcfdfe3c8eee71
Reed <mrnitsuj1@gmail.com>
2021-03-11 18:05:45 -0500

    ci.nix: remove unnecessary home-manager build arg

--- ci.nix
+++ ci.nix
@@ -8,6 +8,6 @@ in
   nixos-t400 = (import "${sources.nixpkgs}/nixos" { configuration = import ./system/nixos-t400/configuration.nix; }).system;
   nixos-t520 = (import "${sources.nixpkgs}/nixos" { configuration = import ./system/nixos-t520/configuration.nix; }).system;
   nixos-desktop = (import "${sources.nixpkgs}/nixos" { configuration = import ./system/nixos-desktop/configuration.nix; }).system;
-  home-manager = (import "${sources.home-manager}/home-manager/home-manager.nix" { pkgs = import "${sources.nixpkgs}" { }; confPath = ./home.nix; }).activationPackage;
+  home-manager = (import "${sources.home-manager}/home-manager/home-manager.nix" { confPath = ./home.nix; }).activationPackage;
 
 }
