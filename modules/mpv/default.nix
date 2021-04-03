b3d2d51523af20e9314f2d48c0a6b3cbf065b875
Reed <mrnitsuj1@gmail.com>
2021-04-03 01:28:50 -0400

    fix mpv-webm
    Former-commit-id: f8f60dfc71f3f8d7a971b4442dcab5c1204e53cb

--- modules/mpv/default.nix
+++ modules/mpv/default.nix
@@ -3,6 +3,19 @@ let
 
   sources = import ./nix/sources.nix;
 
+  mpv-webm = pkgs.stdenv.mkDerivation {
+    name = "mpv-webm";
+    src = sources.mpv-webm;
+
+    nativeBuildInputs = [ pkgs.luaPackages.moonscript ];
+
+    installPhase = ''
+      mkdir -p $out/build
+      cp -v build/* $out/build
+    '';
+
+  };
+
 in
 {
   programs.mpv = {
@@ -32,7 +45,7 @@ in
       };
     in
     {
-      "mpv/scripts/webm.lua".source = "${sources.mpv-webm}/build/webm.lua";
+      "mpv/scripts/webm.lua".source = "${mpv-webm}/build/webm.lua";
       "mpv/scripts/mpv_thumbnail_script_client_osc.lua".source = "${mpv_thumbnail_script}/mpv_thumbnail_script_client_osc.lua";
       "mpv/scripts/mpv_thumbnail_script_server.lua".source = "${mpv_thumbnail_script}/mpv_thumbnail_script_server.lua";
       "mpv/script-opts/mpv_thumbnail_script.conf".text = ''
