ec5e572be1bda1aec3d28d9b2b981a7b84affee4
Reed <mrnitsuj1@gmail.com>
2021-04-11 20:49:41 -0400

    ranger: use pypy3

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -32,4 +32,11 @@ self: super: {
       ];
     }
   );
+  ranger = super.ranger.overrideAttrs (
+    old: rec {
+      postFixup = old.postFixup + ''
+        sed -i "s_#!/nix/store/.*_#!${super.pypy3}/bin/pypy3_" $out/bin/.ranger-wrapped
+      '';
+    }
+  );
 }
