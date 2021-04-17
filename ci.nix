16a54c8e0abf46685027bcee3bf18f5890ed7cbe
Reed <mrnitsuj1@gmail.com>
2021-04-17 17:48:26 -0400

    add generic configuration.nix

--- ci.nix
+++ ci.nix
@@ -5,6 +5,7 @@ let
 in
 {
 
+  nixos = (import "${sources.nixpkgs}/nixos" { configuration = import ./system/nixos/configuration.nix; }).system;
   nixos-t400 = (import "${sources.nixpkgs}/nixos" { configuration = import ./system/nixos-t400/configuration.nix; }).system;
   nixos-t520 = (import "${sources.nixpkgs}/nixos" { configuration = import ./system/nixos-t520/configuration.nix; }).system;
   nixos-desktop = (import "${sources.nixpkgs}/nixos" { configuration = import ./system/nixos-desktop/configuration.nix; }).system;
---
+++ system/nixos/configuration.nix
@@ -0,0 +1,82 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      /etc/nixos/cachix.nix
+      # Include the results of the hardware scan.
+      /etc/nixos/hardware-configuration.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub = {
+    enable = true;
+    version = 2;
+    # Define on which hard drive you want to install Grub.
+    device = "/dev/vda"; # or "nodev" for efi only
+  };
+
+  networking = {
+    hostName = "nixos"; # Define your hostname.
+    networkmanager.enable = true;
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  services.xserver = {
+    enable = true;
+    displayManager = {
+      autoLogin = {
+        enable = true;
+        user = "reed";
+      };
+      lightdm = {
+        enable = true;
+        greeter.enable = false;
+      };
+      session = [
+        {
+          manage = "desktop";
+          name = "xsession";
+          start = ''exec $HOME/.xsession'';
+        }
+      ];
+    };
+  };
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.reed = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    shell = pkgs.zsh;
+  };
+
+  programs.gnupg.agent = {
+    enable = true;
+    pinentryFlavor = "tty";
+  };
+  environment.pathsToLink = [ "/share/zsh" ];
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.05"; # Did you read the comment?
+
+}
