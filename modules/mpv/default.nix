fbc2a01a1132f2a1738db4f4a1e12969fd0f132f
Reed <mrnitsuj1@gmail.com>
2021-04-24 11:47:56 -0400

    remove unneeded verbosity from build output

--- modules/mpv/default.nix
+++ modules/mpv/default.nix
@@ -11,11 +11,25 @@ let
 
     installPhase = ''
       mkdir -p $out/build
-      cp -v build/* $out/build
+      cp build/* $out/build
     '';
 
   };
+  mpv_thumbnail_script = pkgs.stdenv.mkDerivation {
+    name = "mpv_thumbnail_script";
+    src = sources.mpv_thumbnail_script;
 
+    nativeBuildInputs = [ pkgs.python3 ];
+
+    patchPhase = ''
+      patchShebangs ./concat_files.py
+    '';
+
+    installPhase = ''
+      mkdir -p "$out"
+      cp *.lua "$out"
+    '';
+  };
 in
 {
   programs.mpv = {
@@ -26,31 +40,13 @@ in
       osc = "no";
     };
   };
-  xdg.configFile =
-    let
-      mpv_thumbnail_script = pkgs.stdenv.mkDerivation {
-        name = "mpv_thumbnail_script";
-        src = sources.mpv_thumbnail_script;
-
-        nativeBuildInputs = [ pkgs.python3 ];
-
-        patchPhase = ''
-          patchShebangs ./concat_files.py
-        '';
-
-        installPhase = ''
-          mkdir -p "$out"
-          cp -v *.lua "$out"
-        '';
-      };
-    in
-    {
-      "mpv/scripts/webm.lua".source = "${mpv-webm}/build/webm.lua";
-      "mpv/scripts/mpv_thumbnail_script_client_osc.lua".source = "${mpv_thumbnail_script}/mpv_thumbnail_script_client_osc.lua";
-      "mpv/scripts/mpv_thumbnail_script_server.lua".source = "${mpv_thumbnail_script}/mpv_thumbnail_script_server.lua";
-      "mpv/script-opts/mpv_thumbnail_script.conf".text = ''
-        mpv_no_sub=yes
-        autogenerate_max_duration=0
-      '';
-    };
+  xdg.configFile = {
+    "mpv/scripts/webm.lua".source = "${mpv-webm}/build/webm.lua";
+    "mpv/scripts/mpv_thumbnail_script_client_osc.lua".source = "${mpv_thumbnail_script}/mpv_thumbnail_script_client_osc.lua";
+    "mpv/scripts/mpv_thumbnail_script_server.lua".source = "${mpv_thumbnail_script}/mpv_thumbnail_script_server.lua";
+    "mpv/script-opts/mpv_thumbnail_script.conf".text = ''
+      mpv_no_sub=yes
+      autogenerate_max_duration=0
+    '';
+  };
 }
