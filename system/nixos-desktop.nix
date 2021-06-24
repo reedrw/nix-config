72e367407f8efeea86d5a1992f42f35aa2fdab98
Reed <mrnitsuj1@gmail.com>
2021-06-24 18:01:49 -0400

    nixos-desktop: use flakes

--- system/nixos-desktop.nix
+++ system/nixos-desktop.nix
@@ -49,8 +49,14 @@ in
   };
 
   nix = {
+    package = pkgs.nixUnstable.overrideAttrs (
+      old: {
+        src = sources.nix;
+      }
+    );
     autoOptimiseStore = true;
     extraOptions = ''
+      experimental-features = nix-command flakes
       keep-outputs = true
       keep-derivations = true
     '';
