234fa70535204b8af6d0b88704d7210707f5304c
Reed <mrnitsuj1@gmail.com>
2021-07-28 14:38:02 -0400

    nixos-desktop: block genshin trackers

--- system/nixos-desktop.nix
+++ system/nixos-desktop.nix
@@ -196,6 +196,18 @@ in
   # }}}
   # {{{ Networking
   networking = {
+    extraHosts = ''
+      # Genshin logging servers (do not remove!)
+      0.0.0.0 log-upload-os.mihoyo.com
+      0.0.0.0 overseauspider.yuanshen.com
+
+      # Optional Unity proxy/cdn servers
+      0.0.0.0 prd-lender.cdp.internal.unity3d.com
+      0.0.0.0 thind-prd-knob.data.ie.unity3d.com
+      0.0.0.0 thind-gke-usc.prd.data.corp.unity3d.com
+      0.0.0.0 cdp.cloud.unity3d.com
+      0.0.0.0 remote-config-proxy-prd.uca.cloud.unity3d.com
+    '';
     networkmanager.enable = true;
     hostName = "nixos-desktop";
     firewall.allowedTCPPorts = [ 2049 ];
