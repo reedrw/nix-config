da8a27bffdca1e75e0a377a09e04513e0868b745
Reed <mrnitsuj1@gmail.com>
2021-09-11 18:25:38 -0400

    new nix-index-database release format

--- modules/comma/update-sources.sh
+++ modules/comma/update-sources.sh
@@ -8,7 +8,7 @@ tag=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --tags --sort='
   https://github.com/Mic92/nix-index-database | \
   awk 'END {match($2, /([^/]+)$/, m); print m[0]}')
 
-url="https://github.com/Mic92/nix-index-database/releases/download/$tag/files"
+url="https://github.com/Mic92/nix-index-database/releases/download/$tag/index-x86_64-linux"
 
 sha256="$(nix-prefetch-url "$url")"
 
