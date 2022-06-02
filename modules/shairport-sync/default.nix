41daa27388e2c0efeede6f5de714fe19e48e1847
Reed <mrnitsuj1@gmail.com>
2022-06-01 23:39:42 -0400

    shairport-sync: fix mpris

--- modules/shairport-sync/default.nix
+++ modules/shairport-sync/default.nix
@@ -1,8 +1,19 @@
 { config, lib, pkgs, ... }:
 let
+  # When https://github.com/NixOS/nixpkgs/pull/175860 hits unstable
+  # remove below
   shairport-sync = pkgs.shairport-sync.overrideAttrs (
     old: {
-      configureFlags = old.configureFlags ++ [ "--with-metadata" ];
+      buildInputs = old.buildInputs ++ [ pkgs.glib ];
+      configureFlags = old.configureFlags ++ [
+        "--with-dbus-interface"
+        "--with-mpris-interface"
+      ];
+
+      prePatch = ''
+        sed 's/G_BUS_TYPE_SYSTEM/G_BUS_TYPE_SESSION/g' -i dbus-service.c
+        sed 's/G_BUS_TYPE_SYSTEM/G_BUS_TYPE_SESSION/g' -i mpris-service.c
+      '';
     }
   );
 in
