9360034a5b19803b890091c225c0602bba4f51c3
Reed <mrnitsuj1@gmail.com>
2022-06-17 23:31:52 -0400

    alacritty: fix terminal colors

--- modules/alacritty/theme.nix
+++ modules/alacritty/theme.nix
@@ -28,13 +28,13 @@ colors:
   # Bright colors
   bright:
     black:   '0x${base03}'
-    red:     '0x${base09}'
-    green:   '0x${base01}'
-    yellow:  '0x${base02}'
-    blue:    '0x${base04}'
-    magenta: '0x${base06}'
-    cyan:    '0x${base0F}'
-    white:   '0x${base07}'
+    red:     '0x${base08}'
+    green:   '0x${base0B}'
+    yellow:  '0x${base0A}'
+    blue:    '0x${base0D}'
+    magenta: '0x${base0E}'
+    cyan:    '0x${base0C}'
+    white:   '0x${base05}'
 
 draw_bold_text_with_bright_colors: false
 ''
