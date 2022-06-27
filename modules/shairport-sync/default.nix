432d9ef9122eca2adfe3990322fe7a042fec1d93
Reed <mrnitsuj1@gmail.com>
2022-06-27 06:52:20 -0400

    shairport-sync: remove override

--- modules/shairport-sync/default.nix
+++ modules/shairport-sync/default.nix
@@ -1,22 +1,4 @@
 { config, lib, pkgs, ... }:
-let
-  # When https://github.com/NixOS/nixpkgs/pull/175860 hits unstable
-  # remove below
-  shairport-sync = pkgs.shairport-sync.overrideAttrs (
-    old: {
-      buildInputs = old.buildInputs ++ [ pkgs.glib ];
-      configureFlags = old.configureFlags ++ [
-        "--with-dbus-interface"
-        "--with-mpris-interface"
-      ];
-
-      prePatch = ''
-        sed 's/G_BUS_TYPE_SYSTEM/G_BUS_TYPE_SESSION/g' -i dbus-service.c
-        sed 's/G_BUS_TYPE_SYSTEM/G_BUS_TYPE_SESSION/g' -i mpris-service.c
-      '';
-    }
-  );
-in
 {
   systemd.user.services.shairport-sync = {
     Unit = {
@@ -28,7 +10,7 @@ in
     };
     Service = {
       Environment = "PATH=${config.home.profileDirectory}/bin";
-      ExecStart = "${shairport-sync}/bin/shairport-sync -v -o pa";
+      ExecStart = "${pkgs.shairport-sync}/bin/shairport-sync -v -o pa";
       ExecStop = "${pkgs.procps}/bin/pkill shairport-sync";
       Restart= "on-failure";
       Type = "simple";
