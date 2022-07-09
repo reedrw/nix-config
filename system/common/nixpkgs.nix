f9db214c8383b2a2845e33221bebe29b34c290c2
Reed <mrnitsuj1@gmail.com>
2022-07-09 19:14:39 -0400

    nixpkgs: new settings

--- system/common/nixpkgs.nix
+++ system/common/nixpkgs.nix
@@ -15,18 +15,17 @@ in
 
   nix = {
     autoOptimiseStore = true;
+    settings = {
+      trusted-users = [ "root" "@wheel" ];
+      substituters = [ "https://reedrw.cachix.org" ];
+      trusted-public-keys = [
+        "reedrw.cachix.org-1:do9gZInXOYTRPYU+L/x7B90hu1usmnaSFGJl6PN7NC4="
+      ];
+    };
     extraOptions = ''
       keep-outputs = true
       keep-derivations = true
       experimental-features = nix-command flakes
     '';
-    trustedUsers = [ "root" "@wheel" ];
-
-    binaryCaches = [
-      "https://reedrw.cachix.org"
-    ];
-    binaryCachePublicKeys = [
-      "reedrw.cachix.org-1:do9gZInXOYTRPYU+L/x7B90hu1usmnaSFGJl6PN7NC4="
-    ];
   };
 }
