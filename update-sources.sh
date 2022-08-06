a653dfe8dbc27cf2c43430bc418288c3463de6d5
Reed <mrnitsuj1@gmail.com>
2022-08-06 16:43:48 -0400

    ./update.sh: start tracking aagl cargoSha256

--- update-sources.sh
+++ update-sources.sh
@@ -1,8 +1,28 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p niv
+#! nix-shell -i bash -p niv nix-prefetch gron
 
 PS4=''
 set -x
 
 niv init
 niv update
+
+gronnedJson="$(gron ./nix/sources.json)"
+# shellcheck disable=SC2016
+aaglCargoSha="$(nix-prefetch '
+  { sha256 }:
+  let
+    sources = import ./nix/sources.nix { };
+    nur = import "${sources.NUR}" {
+      inherit pkgs;
+    };
+  in
+  nur.repos.reedrw.an-anime-game-launcher-gtk-unwrapped.cargoDeps.overrideAttrs (_:
+    {
+      src = sources.an-anime-game-launcher-gtk;
+      cargoSha256 = sha256;
+    }
+  )
+')"
+gronnedJson="$gronnedJson"$'\n'"json[\"an-anime-game-launcher-gtk\"].cargoSha256 = \"$aaglCargoSha\";"
+gron -u <<< "$gronnedJson" > ./nix/sources.json
