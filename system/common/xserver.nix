45173562e3791c941319e7a2f03ecace110b5eac
Reed <mrnitsuj1@gmail.com>
2022-08-11 14:38:14 -0400

    xserver: dark mode as root

--- system/common/xserver.nix
+++ system/common/xserver.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }:
-
+let
+  sources = import ../../nix/sources.nix { };
+in
 {
+  imports = [
+    "${sources.home-manager}/nixos"
+  ];
   programs.dconf.enable = true;
   services.xserver = {
     enable = true;
@@ -21,5 +26,14 @@
         }
       ];
     };
+  };
+  # Dark mode when running apps in sudo
+  home-manager.users.root = {
+    home = {
+      username = "root";
+      homeDirectory = "/root";
+      stateVersion = "22.05";
+    };
+    imports = [ ../../modules/styling ];
   };
 }
