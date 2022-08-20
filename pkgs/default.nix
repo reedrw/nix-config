258e0df565bfccd07e42f18e767c101921476e1a
Reed <mrnitsuj1@gmail.com>
2022-08-20 11:57:34 -0400

    pkgs: pin mupdf to 1.19.0

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -16,4 +16,10 @@ self: super: {
   discord-canary = super.discord-canary.override {
     nss = super.nss_latest;
   };
+  # Pin mupdf to 1.19.0
+  # https://github.com/NixOS/nixpkgs/issues/187305
+  mupdf = (import (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/9050439c4c1a485a12c5f7754a94ae35dda2699b.tar.gz";
+    sha256 = "0pdrr06rzffh08ys3kkanbirjgnawqzxzi61kxdd83z04ndvz2bl";
+  }) {}).mupdf;
 }
