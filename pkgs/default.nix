2efa7ec214f2c64db29e1157b125235b03d62132
Reed <mrnitsuj1@gmail.com>
2022-09-04 14:18:27 -0400

    discord-canary: broken yet again

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -13,8 +13,8 @@ self: super: {
   )).override {
     nss = super.nss_latest;
   };
-  # https://github.com/NixOS/nixpkgs/pull/189044
-  discord-canary = super.discord-canary.override {
+  # https://github.com/NixOS/nixpkgs/pull/189549
+  discord-canary = (import (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/1a03c4a771e8d09b25996f56b9ceafb2aaa8ee4a.tar.gz) {}).discord-canary.override {
     nss = super.nss_latest;
   };
 }
