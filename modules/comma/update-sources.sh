0787ead0d5a3a637a610c88cb01d91321e11c73c
Reed <mrnitsuj1@gmail.com>
2022-09-26 00:03:21 -0400

    comma: don't update nix-index cache if no new index

--- modules/comma/update-sources.sh
+++ modules/comma/update-sources.sh
@@ -1,14 +1,18 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i bash -p coreutils curl gawk git
+#! nix-shell -i bash -p coreutils curl gawk git jq
 
-PS4=''
-set -x
+savedUrl="$(jq -r '.url' ./source.json)"
 
 tag=$(git -c 'versionsort.suffix=-' ls-remote --exit-code --refs --tags --sort='v:refname' \
   https://github.com/Mic92/nix-index-database | \
   awk 'END {match($2, /([^/]+)$/, m); print m[0]}')
 
 url="https://github.com/Mic92/nix-index-database/releases/download/$tag/index-x86_64-linux"
+
+if [[ "$url" == "$savedUrl" ]]; then
+  echo "No nix-index update needed"
+  exit 0
+fi
 
 sha256="$(nix-prefetch-url "$url")"
 
