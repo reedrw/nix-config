8d411fb91d6910da918c759bd623470d6cae3483
Reed <mrnitsuj1@gmail.com>
2022-10-02 19:56:55 -0400

    i3-gaps: downgrade to 4.20.1

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -22,4 +22,7 @@ self: super: {
   );
   # https://github.com/NixOS/nixpkgs/pull/193868
   fzf = (import (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/c779b5e306e46b9c571f798aa7d4ce1285239151.tar.gz) {}).fzf;
+
+  # https://github.com/i3/i3/issues/5184
+  i3-gaps = (import (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/f4edfa5f90abd526d563da66c84c29723deb3e9b.tar.gz) {}).i3-gaps;
 }
