cb37ed0403670e8a88b07df6d4dc6125956f2112
Reed <mrnitsuj1@gmail.com>
2022-10-19 18:18:59 -0400

    xsession: fix unalix update script

--- modules/xsession/scripts/clipboard-clean-patches/update-sources.sh
+++ modules/xsession/scripts/clipboard-clean-patches/update-sources.sh
@@ -6,17 +6,17 @@ set -x
 niv init
 niv update
 
-rev="$(jq -r '.[Unalix].rev' ./nix/sources.json)"
+rev="$(jq -r '.["Unalix"].rev' ./nix/sources.json)"
 
 dir="$(mktemp -d)"
 
 pushd "$dir" || exit
   git clone https://github.com/AmanoTeam/Unalix
-  git checkout "$rev"
   pushd Unalix || exit
+    git checkout "$rev"
     python3 ./external/update_ca_bundle.py
     python3 ./external/update_rules_file.py
-    git diff | tee ./update.patch
+    git diff > ./update.patch
   popd || exit
 popd || exit
 
