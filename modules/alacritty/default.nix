cbdc485ef66ba380d98634e84040cef51de41d33
Reed <mrnitsuj1@gmail.com>
2022-10-23 13:51:07 -0400

    alacritty: move all config to nix

--- modules/alacritty/default.nix
+++ modules/alacritty/default.nix
@@ -14,45 +14,74 @@ in
 
   home.packages = [ pkgs.scientifica ]; # font
 
-  programs.alacritty.enable = true;
-  xdg.configFile."alacritty/alacritty.yml".text = ''
-    import:
-      - ${import ./theme.nix { inherit config; } }
-
-    live_config_reload: true
-
-    cursor:
-      style: Underline
-
-    font:
-      normal:
-        family: scientifica
-        style: Medium
-
-      bold:
-        family: scientifica
-        style: Bold
-
-      italic:
-        family: scientifica
-        style: Italic
-
-      bold_italic:
-        family: scientifica
-        style: Bold
-
-      size: 8
-
-    window:
-      dynamic_padding: true
-      padding:
-        x: 15
-        y: 15
-
-    shell:
-      program: ${pkgs.tmux}/bin/tmux
-      args:
-        - -f
-        - ${tmuxconf}
-  '';
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      live_config_reload = true;
+      # copied from
+      # https://github.com/aarowill/base16-alacritty/blob/master/templates/default.mustache
+      colors = with config.colorScheme.colors; let
+        black   = "0x${base00}";
+        blue    = "0x${base0D}";
+        cyan    = "0x${base0C}";
+        green   = "0x${base0B}";
+        grey    = "0x${base03}";
+        magenta = "0x${base0E}";
+        red     = "0x${base08}";
+        white   = "0x${base05}";
+        yellow  = "0x${base0A}";
+      in {
+        primary = {
+          background = black;
+          foreground = white;
+        };
+        cursor = {
+          text = black;
+          cursor = white;
+        };
+        normal = {
+          inherit black blue cyan green magenta red white yellow;
+        };
+        bright = {
+          black = grey;
+          inherit blue cyan green magenta red white yellow;
+        };
+        draw_bold_text_with_bright_colors = false;
+      };
+      cursor.style = "Underline";
+      font = {
+        size = 8;
+        normal = {
+          family = "scientifica";
+          style = "Medium";
+        };
+        bold = {
+          family = "scientifica";
+          style = "Bold";
+        };
+        italic = {
+          family = "scientifica";
+          style = "Italic";
+        };
+        bold_italic = {
+          family = "scientifica";
+          style = "Bold";
+        };
+      };
+      window = {
+        dynamic_padding = true;
+        padding = {
+          x = 15;
+          y = 15;
+        };
+      };
+      shell = {
+        program = "${pkgs.tmux}/bin/tmux";
+        args = [
+          "-f"
+          "${tmuxconf}"
+        ];
+      };
+    };
+  };
 }
