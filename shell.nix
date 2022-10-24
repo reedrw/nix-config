08f93841f8fcae22935200243c626946f0aa88d9
Reed <mrnitsuj1@gmail.com>
2022-10-24 15:37:02 -0400

    shell.nix: use mkShell

--- shell.nix
+++ shell.nix
@@ -9,10 +9,65 @@ let
 
   pkgs = import sources.nixpkgs {
     overlays = [
-      nur.repos.reedrw.overlays.mkYamlShell
       hm-overlay
       (import ./pkgs)
     ];
+    config = import ./config.nix;
   };
+
+  aliasToPackage = alias: (
+    pkgs.lib.mapAttrsToList
+    (name: value: pkgs.writeShellScriptBin name value)
+    alias
+  );
 in
-pkgs.mkYamlShell ./shell.yml
+with pkgs;
+mkShell {
+  name = "nix-config";
+  packages = [
+    cargo
+    doppler
+    gcc
+    git
+    gron
+    home-manager
+    jq
+    niv
+    nix-prefetch
+    pre-commit
+    shellcheck
+    wget
+
+    (symlinkJoin {
+      name = "alias";
+      paths = aliasToPackage {
+        build = ''
+          export NIXPKGS_ALLOW_UNFREE=1
+          ci="$(git rev-parse --show-toplevel)/ci.nix"
+          if [[ -z "$1" ]]; then
+            nix shell nixpkgs#nix-output-monitor -c nom-build "$ci"
+          else
+            nix shell nixpkgs#nix-output-monitor -c nom-build "$ci" -A "$1"
+          fi
+        '';
+        update-all = ''
+          find -L "$(pwd)/" -type f -name "update-sources.sh" \
+          | while read -r updatescript; do
+            (
+              dir="$(dirname -- "$updatescript")"
+              cd "$dir" || exit
+              $updatescript
+            )
+          done
+        '';
+      };
+    })
+  ];
+
+  PRE_COMMIT_COLOR = "never";
+  SHELLCHECK_OPTS = "-e SC1008";
+
+  shellHook = ''
+    [[ -f .git/hooks/pre-commit ]] || pre-commit install --install-hooks
+  '';
+}
