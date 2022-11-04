14803b2878af2e16380b41d6c8d934b9b7b0f166
Reed <mrnitsuj1@gmail.com>
2022-11-04 17:42:12 -0400

    ci.nix: home-manager should inherit pkgs

--- ci.nix
+++ ci.nix
@@ -1,6 +1,6 @@
 {
   sources ? import ./nix/sources.nix { },
-  pkgs ? import sources.nixpkgs { }
+  pkgs ? import sources.nixpkgs { config = import ./config.nix; }
 }:
 
 {
@@ -10,6 +10,6 @@
   nixos-t480 = (import "${sources.nixpkgs}/nixos" { configuration = import ./system/nixos-t480.nix; }).system;
   nixos-t520 = (import "${sources.nixpkgs}/nixos" { configuration = import ./system/nixos-t520.nix; }).system;
   nixos-desktop = (import "${sources.nixpkgs}/nixos" { configuration = import ./system/nixos-desktop.nix; }).system;
-  home-manager = (import "${sources.home-manager}/home-manager/home-manager.nix" { confPath = ./home.nix; }).activationPackage;
+  home-manager = (import "${sources.home-manager}/home-manager/home-manager.nix" { confPath = ./home.nix; inherit pkgs; }).activationPackage;
 
 }
