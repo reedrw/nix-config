bf09e015a1a7c37a456e2dc2e99498428d7261bc
Reed <mrnitsuj1@gmail.com>
2022-12-01 15:01:15 -0500

    nvim: move cursor by display lines when wrapping, - also add line wrapping toggle

--- modules/nvim/default.nix
+++ modules/nvim/default.nix
@@ -176,6 +176,41 @@ in
       inoremap <expr><S-TAB> coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"
       inoremap <silent><expr> <c-space> coc#refresh()
 
+      " https://vim.fandom.com/wiki/Move_cursor_by_display_lines_when_wrapping
+      noremap <silent> <Leader>w :call ToggleWrap()<CR>
+      function ToggleWrap()
+        if &wrap
+          echo "Wrap OFF"
+          setlocal nowrap
+          set virtualedit=all
+          silent! nunmap <buffer> <Up>
+          silent! nunmap <buffer> <Down>
+          silent! nunmap <buffer> <Home>
+          silent! nunmap <buffer> <End>
+          silent! iunmap <buffer> <Up>
+          silent! iunmap <buffer> <Down>
+          silent! iunmap <buffer> <Home>
+          silent! iunmap <buffer> <End>
+        else
+          echo "Wrap ON"
+          setlocal wrap linebreak nolist
+          set virtualedit=
+          setlocal display+=lastline
+          noremap  <buffer> <silent> <Up>   gk
+          noremap  <buffer> <silent> <Down> gj
+          noremap  <buffer> <silent> <Home> g<Home>
+          noremap  <buffer> <silent> <End>  g<End>
+          inoremap <buffer> <silent> <Up>   <C-o>gk
+          inoremap <buffer> <silent> <Down> <C-o>gj
+          inoremap <buffer> <silent> <Home> <C-o>g<Home>
+          inoremap <buffer> <silent> <End>  <C-o>g<End>
+        endif
+      endfunction
+      noremap <silent> k gk
+      noremap <silent> j gj
+      noremap <silent> 0 g0
+      noremap <silent> $ g$
+
       function! s:check_back_space() abort
         let col = col('.') - 1
         return !col || getline('.')[col - 1]  =~# '\s'
