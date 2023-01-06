a5bdfc76a3cac2f75d8b61332f2a6922b04101c2
Reed <mrnitsuj1@gmail.com>
2023-01-05 19:00:13 -0500

    zsh: try to use your own aliases that you made for yourself, genius

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -214,9 +214,9 @@ in
       mkdir = "mkdir -vp";
       mv = "mv -iv";
       ping = "${binPath prettyping} --nolegend";
-      ranger = "ranger_cd";
+      ranger = "echo 'You already have an alias for this! Use `rr` instead'";
       rm = "rm -v";
-      rr = "ranger";
+      rr = "ranger_cd";
       rsync = "rsync --old-args";
       tb = "termbin";
       termbin = "nc termbin.com 9999";
