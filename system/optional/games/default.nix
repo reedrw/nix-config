9e52ef3dbaf1d773e164b9a9cc5044254685395e
Reed <mrnitsuj1@gmail.com>
2023-03-04 14:36:40 -0500

    manual update

--- system/optional/games/default.nix
+++ system/optional/games/default.nix
@@ -3,10 +3,10 @@ let
   sources = import ./nix/sources.nix { };
   aagl-gtk-on-nix = import sources.aagl-gtk-on-nix { inherit pkgs; };
 
-  aagl-gtk-unwrapped = aagl-gtk-on-nix.an-anime-game-launcher-gtk-unwrapped-git;
+  aagl-gtk-unwrapped = aagl-gtk-on-nix.an-anime-game-launcher-unwrapped-git;
 
   aagl-gtk-custom = aagl-gtk-on-nix.an-anime-game-launcher-gtk.override {
-    an-anime-game-launcher-gtk-unwrapped = with (lib.importJSON ./components.json); aagl-gtk-unwrapped.override {
+    an-anime-game-launcher-unwrapped = with (lib.importJSON ./components.json); aagl-gtk-unwrapped.override {
       customDxvk = dxvk;
       customGEProton = GEProton;
       customSoda = soda;
