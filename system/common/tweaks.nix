d806414c4b1d859d1b2c37b9ca4052c6fe38576e
Reed <mrnitsuj1@gmail.com>
2023-03-17 14:36:51 -0400

    system: tweaks: fix sudo TTY hijack vulnerability

--- system/common/tweaks.nix
+++ system/common/tweaks.nix
@@ -4,6 +4,12 @@
   services.journald.extraConfig = "SystemMaxUse=500M";
   services.udisks2.enable = true;
 
+  security.sudo.extraConfig = ''
+    # Prevent arbitrary code execution as your user when sudoing to another
+    # user due to TTY hijacking via TIOCSTI ioctl.
+    Defaults use_pty
+  '';
+
   systemd = let
     extraConfig = ''
       DefaultTimeoutStartSec=30s
