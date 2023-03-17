a6e475fa91696b253e5b49639e27ce3c1c3d08f2
Reed <mrnitsuj1@gmail.com>
2023-03-17 02:14:17 -0400

    xsession: add notification to volume script; more robust

--- modules/xsession/scripts/volume.sh
+++ modules/xsession/scripts/volume.sh
@@ -1,25 +1,50 @@
 #! /usr/bin/env bash
 
+set -x
+
 cacheDir="$HOME/.cache"
 cachefile="$cacheDir/outputdev"
 idCacheFile="$cacheDir/outputid"
+descCacheFile="$cacheDir/outputdesc"
+
+notifyCommand="dunstify -h string:x-dunst-stack-tag:volume"
+
+getInfo(){
+  mkdir -p "$cacheDir"
+  echo "$outputdev" > "$cachefile"
+  outputdesc="$(pactl list sinks | grep -e 'node.name' -e 'device.description' | grep -vi 'effects' | cut -f2 -d\" | grep -B 1 "$outputdev" | grep -v "$outputdev")"
+  echo "$outputdesc" > "$descCacheFile"
+  outputid="$(wpctl status | grep "Sinks" -A 20 | grep "$outputdesc" | grep -o -E '[0-9]+' | head -1)"
+  echo "$outputid" > "$idCacheFile"
+}
+
+runCommand(){
+  local runCommand="$1"
+  local retry=0
+
+  # device IDs sometimes change for seemingly no reason, so retry once if it fails.
+  $runCommand || retry=1
+  if [[ "$retry" == 1 ]]; then
+    getInfo && $runCommand
+  fi
+}
 
 outputdev="$(gsettings \
   --schemadir ~/.nix-profile/share/gsettings-schemas/easyeffects-7.0.0/glib-2.0/schemas \
   get com.github.wwmm.easyeffects.streamoutputs output-device | cut -f2 -d\')"
 
-if [[ -f "$cachefile" && -f "$idCacheFile" ]] && [[ "$(< "$cachefile")" == "$outputdev" ]]; then
+if   [[ -f "$cachefile" && -f "$idCacheFile" && -f "$descCacheFile" ]] \
+  && [[ "$(< "$cachefile")" == "$outputdev" ]]; then
   outputid="$(< "$idCacheFile")"
+  outputdesc="$(< "$descCacheFile")"
 else
-  mkdir -p "$cacheDir"
-  echo "$outputdev" > "$cachefile"
-  outputdesc="$(pactl list sinks | grep -e 'node.name' -e 'device.description' | grep -vi 'effects' | cut -f2 -d\" | grep -B 1 "$outputdev" | grep -v "$outputdev")"
-  outputid="$(wpctl status | grep "Sinks" -A 20 | grep "$outputdesc" | grep -o -E '[0-9]+' | head -1)"
-  echo "$outputid" > "$idCacheFile"
+  getInfo
 fi
 
 if [[ "$1" == "mute" ]]; then
-  wpctl set-mute "$outputid" toggle
+  command="wpctl set-mute $outputid toggle"
+  runCommand "$command"
+  $notifyCommand "$outputdesc" "$(wpctl get-volume "$outputid")"
   exit "$?"
 fi
 
@@ -29,4 +54,6 @@ else
   pm="-"
 fi
 
-wpctl set-volume "$outputid" "$2"%"$pm"
+command="wpctl set-volume $outputid $2%$pm"
+runCommand "$command"
+$notifyCommand "$outputdesc" "$(wpctl get-volume "$outputid")"
