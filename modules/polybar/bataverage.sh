d9a1619f2fe9af0fd04043da2926a6463c0d7ecb
Reed <mrnitsuj1@gmail.com>
2023-04-27 15:13:15 -0400

    polybar: bataverage desktop fix

--- modules/polybar/bataverage.sh
+++ modules/polybar/bataverage.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-acpi | sed -e 's/%.*$//g' -e 's/^.*, //g' | awk '{ sum += $1; n++ } END { if (n > 0) printf "%d%\n", int(sum / n); }'
-sleep 3;
+if [[ "$(hostname)" != *"desktop"* ]]; then
+  acpi | sed -e 's/%.*$//g' -e 's/^.*, //g' | awk '{ sum += $1; n++ } END { if (n > 0) printf "%d%\n", int(sum / n); }'
+else sleep 99999999999
+fi
+sleep 10
