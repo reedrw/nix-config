c0763ca99622a655003c3830197a0fcb7d34dc66
Reed <mrnitsuj1@gmail.com>
2023-05-05 14:19:10 -0400

    polybar: bataverage speep infinity

--- modules/polybar/bataverage.sh
+++ modules/polybar/bataverage.sh
@@ -2,6 +2,6 @@
 
 if [[ "$(hostname)" != *"desktop"* ]]; then
   acpi | sed -e 's/%.*$//g' -e 's/^.*, //g' | awk '{ sum += $1; n++ } END { if (n > 0) printf "%d%\n", int(sum / n); }'
-else sleep 99999999999
+else sleep infinity
 fi
 sleep 10
