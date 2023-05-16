0eb1741497eb091fa1789379321aae1d256d13d7
Reed <mrnitsuj1@gmail.com>
2023-05-16 18:04:24 -0400

    sshd: fix iOS shortcuts and openssh not agreeing on hashing algo

--- system/common/sshd.nix
+++ system/common/sshd.nix
@@ -6,6 +6,13 @@
     settings = {
       PasswordAuthentication = false;
       PermitRootLogin = "no";
+      Macs = [
+        "hmac-sha2-512-etm@openssh.com"
+        "hmac-sha2-256-etm@openssh.com"
+        "umac-128-etm@openssh.com"
+        "hmac-sha2-256"
+        "hmac-sha2-512"
+      ];
     };
   };
 }
