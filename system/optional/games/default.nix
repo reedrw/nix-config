64231fbf742a06dfbea207eafd0296afb85d740f
Reed <mrnitsuj1@gmail.com>
2023-05-31 18:10:32 -0400

    games: mullvad-exclude hoyos

--- system/optional/games/default.nix
+++ system/optional/games/default.nix
@@ -4,7 +4,9 @@ let
   aaglPkgs = inputs.aagl.packages.x86_64-linux;
   # aaglPkgs = import /home/reed/files/aagl-gtk-on-nix;
 
-  unwrapped = aaglPkgs.anime-game-launcher.unwrapped;
+  aagl-unwrapped = aaglPkgs.anime-game-launcher.unwrapped;
+  hrl-unwrapped = aaglPkgs.honkers-railway-launcher.unwrapped;
+
   # aagl = with aaglPkgs.anime-game-launcher; override {
   #   unwrapped = unwrapped.overrideAttrs (old: rec {
   #     src = inputs.an-anime-game-launcher;
@@ -18,9 +20,16 @@ let
   #     };
   #   });
   # };
+
   aagl = aaglPkgs.anime-game-launcher.override {
-    unwrapped = unwrapped.override {
+    unwrapped = aagl-unwrapped.override {
       customIcon = builtins.fetchurl components.aagl.icon;
+    };
+  };
+
+  hrl = aaglPkgs.honkers-railway-launcher.override {
+    unwrapped = hrl-unwrapped.override {
+      customIcon = builtins.fetchurl components.hrl.icon;
     };
   };
 
@@ -29,8 +38,15 @@ let
   anime-game-launcher = with pkgs; let
     wrapper = aliasToPackage { anime-game-launcher = "${mve} ${aagl}/bin/anime-game-launcher"; };
   in symlinkJoin {
-    inherit (unwrapped) pname version name;
+    inherit (aagl-unwrapped) pname version name;
     paths = with aagl.passthru; [ wrapper icon desktopEntry ];
+  };
+
+  honkers-railway-launcher = with pkgs; let
+    wrapper = aliasToPackage { honkers-railway-launcher = "${mve} ${hrl}/bin/honkers-railway-launcher"; };
+  in symlinkJoin {
+    inherit (hrl-unwrapped) pname version name;
+    paths = with hrl.passthru; [ wrapper icon desktopEntry ];
   };
 in
 {
@@ -57,19 +73,11 @@ in
 
   networking.mihoyo-telemetry.block = true;
 
-  # programs.honkers-railway-launcher = {
-  #   enable = true;
-  #   package = with aaglPkgs.honkers-railway-launcher; override {
-  #     unwrapped = unwrapped.override {
-  #       customIcon = builtins.fetchurl components.hrl.icon;
-  #     };
-  #   };
-  # };
-
   environment.systemPackages = with pkgs; [
     r2mod_cli
     nurPkgs.genshin-account-switcher
     anime-game-launcher
+    # honkers-railway-launcher
     (aliasToPackage {
       gsi = "anime-game-launcher --run-game";
       gas = ''genshin-account-switcher "$@"'';
