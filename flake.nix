20bc0be331ae9bb469e7556fdf6384fbe2a6b9b4
Reed <mrnitsuj1@gmail.com>
2023-06-01 18:49:47 -0400

    flake.nix: new stable branch

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     master.url = "github:nixos/nixpkgs";
-    stable.url = "github:nixos/nixpkgs/nixos-22.11";
+    stable.url = "github:nixos/nixpkgs/nixos-23.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     NUR.url = "github:nix-community/NUR";
     home-manager = {
