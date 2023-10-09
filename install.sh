3ff4d2aed540fd1c4424fb9e0c9a3f7191782ada
Reed <mrnitsuj1@gmail.com>
2023-10-09 12:00:25 -0400

    install.sh: add --boot option

--- install.sh
+++ install.sh
@@ -5,7 +5,11 @@ set -e
 dir="$(dirname "$0")"
 pushd "$dir" > /dev/null || exit
 
-sudo nixos-rebuild switch --flake "$dir/.#" -L
-[[ "$USER" != "root" ]] && home-manager switch -L
+if [[ "$1" == "--boot" ]]; then
+  sudo nixos-rebuild boot --flake "$dir/.#" -L
+else
+  sudo nixos-rebuild switch --flake "$dir/.#" -L
+  [[ "$USER" != "root" ]] && home-manager switch -L
+fi
 
 popd > /dev/null || exit
