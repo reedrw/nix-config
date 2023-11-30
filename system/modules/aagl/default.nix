7a5e681c8f89bf73ac3806af7e70bceb0f9a84ff
Reed <mrnitsuj1@gmail.com>
2023-11-30 16:54:47 -0500

    games: split AAGL into module

---
+++ system/modules/aagl/default.nix
@@ -0,0 +1,47 @@
+{ config, pkgs, lib, inputs, ... }:
+let
+  cfg = config.custom.aagl;
+  components = lib.importJSON ./components.json;
+  aaglPkgs = inputs.aagl.packages.x86_64-linux;
+in
+{
+  imports = [
+    inputs.aagl.nixosModules.default
+  ];
+
+  options.custom.aagl = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Enable AAGL";
+    };
+
+    mullvad-exclude = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      description = "Exclude AAGL from Mullvad VPN";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.anime-game-launcher = {
+      enable = true;
+      package = with pkgs; let
+        aagl = aaglPkgs.anime-game-launcher.override (old: {
+          unwrapped = old.unwrapped.override {
+            customIcon = builtins.fetchurl components.aagl.icon;
+          };
+        });
+      in
+      if cfg.mullvad-exclude
+      then mullvadExclude aagl
+      else aagl;
+    };
+
+    environment.systemPackages = with pkgs; [
+      (aliasToPackage {
+        gsi = "${config.programs.anime-game-launcher.package}/bin/anime-game-launcher --just-run-game";
+      })
+    ];
+  };
+}
--- system/optional/games/default.nix
+++ system/optional/games/default.nix
@@ -1,32 +1,14 @@
-{ inputs, config, lib, pkgs, ... }:
-let
-  components = lib.importJSON ./components.json;
-  aaglPkgs = inputs.aagl.packages.x86_64-linux;
-in
+{ ... }:
 {
-  imports = [
-    inputs.aagl.nixosModules.default
-  ];
 
   custom.steam = {
     enable = true;
     mullvad-exclude = true;
   };
 
-  programs.anime-game-launcher = {
+  custom.aagl = {
     enable = true;
-    package = pkgs.mullvadExclude (aaglPkgs.anime-game-launcher.override (old: {
-      unwrapped = old.unwrapped.override {
-        customIcon = builtins.fetchurl components.aagl.icon;
-      };
-    }));
+    mullvad-exclude = true;
   };
-
-  environment.systemPackages = with pkgs; [
-    r2mod_cli
-    (aliasToPackage {
-      gsi = "${config.programs.anime-game-launcher.package}/bin/anime-game-launcher --just-run-game";
-    })
-  ];
 
 }
