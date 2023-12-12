80080fe048d059953073dcb9c7dcebbc38f88daa
Reed <mrnitsuj1@gmail.com>
2023-12-11 22:48:08 -0500

    social: fix indentlevel in sources.json

--- home/social/update-sources.sh
+++ home/social/update-sources.sh
@@ -1,9 +1,6 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p jq niv nodejs prefetch-npm-deps
 
-PS4=''
-set -x
-
 currentTag="$(jq -r '.vencord.rev' ./nix/sources.json)"
 
 niv init
@@ -23,9 +20,9 @@ pushd "$tempDir" || exit
   npm install --legacy-peer-deps -f
   npmDepsHash="$(prefetch-npm-deps ./package-lock.json)"
 popd || exit
-cp -r "$tempDir/package-lock.json" ./nix/package-lock.json
+
+tee < "$tempDir/package-lock.json" ./nix/package-lock.json | jq '.'
 rm -rf "$tempDir"
 
-jq --arg npmDepsHash "$npmDepsHash" '.vencord.npmDepsHash = $npmDepsHash' ./nix/sources.json > ./nix/sources.json.tmp
-cat ./nix/sources.json.tmp > ./nix/sources.json
-rm ./nix/sources.json.tmp
+newJson="$(jq --indent 4 --arg npmDepsHash "$npmDepsHash" '.vencord.npmDepsHash = $npmDepsHash' ./nix/sources.json)"
+tee <<< "$newJson" ./nix/sources.json | jq '.'
