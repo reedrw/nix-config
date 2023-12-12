361788bd57eaeac014d475e1b8d9aad84b929fae
Reed <mrnitsuj1@gmail.com>
2023-12-12 14:34:33 -0500

    social: remove unneeded overlay

--- home/social/default.nix
+++ home/social/default.nix
@@ -1,30 +1,29 @@
-{ pkgs, inputs, ... }:
+{ pkgs, ... }:
 let
+  inherit (pkgs.fromBranch) master;
+
   sources = import ./nix/sources.nix { };
-  vencordPkgs = pkgs.importNixpkgs inputs.master {
-    overlays = [ (_: p: {
-      vencord = p.vencord.overrideAttrs (old: rec {
-        version = pkgs.shortenRev sources.vencord.rev;
-        src = sources.vencord;
-        VENCORD_HASH = version;
+  vencord = p: p.vencord.overrideAttrs (old: rec {
+    version = pkgs.shortenRev sources.vencord.rev;
+    src = sources.vencord;
+    VENCORD_HASH = version;
 
-        prePatch = ''
-          cp ${./nix/package-lock.json} package-lock.json
-          chmod +w package-lock.json
-        '';
+    prePatch = ''
+      cp ${./nix/package-lock.json} package-lock.json
+      chmod +w package-lock.json
+    '';
 
-        npmDeps = p.fetchNpmDeps {
-          inherit src prePatch;
-          hash = sources.vencord.npmDepsHash;
-        };
-      });
-    })];
-  };
+    npmDeps = p.fetchNpmDeps {
+      inherit src prePatch;
+      hash = sources.vencord.npmDepsHash;
+    };
+  });
 in
 {
   home.packages = with pkgs; [
     tdesktop
-    (vencordPkgs.discord.override {
+    (master.discord.override {
+      vencord = vencord master;
       withVencord = true;
       nss = nss_latest;
     })
