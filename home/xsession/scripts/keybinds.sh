7eb5972b3f374926f7d9d168d88de865633a9a7e
Reed <mrnitsuj1@gmail.com>
2023-12-27 13:01:05 -0500

    xsession(keybinds): unset caps lock

--- home/xsession/scripts/keybinds.sh
+++ home/xsession/scripts/keybinds.sh
@@ -4,8 +4,12 @@
 set -x
 
 while true; do
+  # set page up and page down to forward and back
   xmodmap -e 'keycode 117 = XF86Forward'
   xmodmap -e 'keycode 112 = XF86Back'
+  # set shift+esc to tilde
   xmodmap -e 'keycode 9 = Escape asciitilde Escape'
+  # unset caps lock
+  xmodmap -e 'keycode 66 = Control_L'
   sleep 20
 done
