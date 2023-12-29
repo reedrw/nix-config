448ec3b2ff0233ee0ec97cf8882bb27ca761a412
Reed <mrnitsuj1@gmail.com>
2023-12-29 04:08:23 -0500

    xsession(clipboard-clean): fix

--- home/xsession/scripts/clipboard-clean-patches/update-sources.sh
+++ home/xsession/scripts/clipboard-clean-patches/update-sources.sh
@@ -15,6 +15,11 @@ pushd "$dir" || exit
   git checkout "$rev"
   python3 ./external/update_ca_bundle.py
   python3 ./external/update_rules_file.py
+
+  # \c isn't supported by python `re` regex.
+  # Very few rules use it (only 1 at the time of writing), so we can just remove it.
+  newJson="$(gron ./unalix/package_data/rulesets/data.min.json | grep -v '\\c' | gron -u)"
+  echo "$newJson" > ./unalix/package_data/rulesets/data.min.json
   git diff > ./update.patch
 popd || exit
 
