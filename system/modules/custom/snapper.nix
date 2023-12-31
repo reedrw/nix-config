1063d826457f665d88c39ab3b00bd13e6c358e36
Reed <mrnitsuj1@gmail.com>
2023-12-30 22:55:51 -0500

    modules(custom): init snapper

---
+++ system/modules/custom/snapper.nix
@@ -0,0 +1,31 @@
+{ lib, config, pkgs, ... }:
+let
+  cfg = config.custom.snapper;
+in
+{
+  options.custom.snapper= {
+    enable = lib.mkEnableOption "enable Snapper";
+
+    allowedUsers = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+      description = "Users allowed to create snapshots";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.snapper = {
+      configs.persist = {
+        SUBVOLUME = "/persist";
+        ALLOW_USERS = cfg.allowedUsers;
+        TIMELINE_CREATE = true;
+        TIMELINE_CLEANUP = true;
+        TIMELINE_LIMIT_HOURLY = 168;
+        TIMELINE_LIMIT_DAILY = 365;
+        TIMELINE_LIMIT_WEEKLY = 100;
+        TIMELINE_LIMIT_MONTHLY = 36;
+        TIMELINE_LIMIT_YEARLY = 0;
+      };
+    };
+  };
+}
