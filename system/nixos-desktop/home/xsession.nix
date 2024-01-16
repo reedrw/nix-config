7c779e8a7cc43b65a54a72d97084fb9f41cb8343
Reed <mrnitsuj1@gmail.com>
2024-01-16 14:28:22 -0500

    nixos-desktop: don't assume which DisplayPort is plugged in

--- system/nixos-desktop/home/xsession.nix
+++ system/nixos-desktop/home/xsession.nix
@@ -4,7 +4,9 @@
   xsession.windowManager.i3.config.startup = let
     alwaysRun = [];
     run = [
+      "xrandr --output DisplayPort-0 --mode 1920x1080 --rate 144"
       "xrandr --output DisplayPort-1 --mode 1920x1080 --rate 144"
+      "xrandr --output DisplayPort-2 --mode 1920x1080 --rate 144"
     ];
   in map ( command: {
     inherit command;
