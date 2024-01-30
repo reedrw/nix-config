2f2f794dfb0e7da4c7165040929604e2e22ec95d
Reed <mrnitsuj1@gmail.com>
2024-01-30 10:59:16 -0500

    home(scripts): delete

--- home/scripts/default.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, ... }:
-{
-  home.packages = with pkgs; [
-    (writeNixShellScript "nivpr" (builtins.readFile ./nivpr.sh))
-    (writeNixShellScript "json2nix" (builtins.readFile ./json2nix.sh))
-  ];
-}
