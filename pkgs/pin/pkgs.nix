c6aef8788afaa11b798b36050a73496ada2eace9
Reed <mrnitsuj1@gmail.com>
2024-01-30 10:58:49 -0500

    pkgs(pin): make cooresponding nixpkgs instance available

--- pkgs/pin/pkgs.nix
+++ pkgs/pin/pkgs.nix
@@ -6,8 +6,10 @@ pkgs: let
     url = "https://github.com/nixos/nixpkgs/archive/${rev}.tar.gz";
   }) { inherit (pkgs) system config; };
 
-  mapToPackage = package: a: lib.mapAttrs' (n: v: lib.nameValuePair
-    ("v" + (builtins.replaceStrings ["."] ["_"] n))
-    (pinnedVersionToPkg v)."${package}"
+  mapToPackage = package: a: lib.mapAttrs' (n: v: let
+      pinnedPkgs = pinnedVersionToPkg v;
+  in lib.nameValuePair
+    ("v" + builtins.replaceStrings ["."] ["_"] n)
+    ({ pkgs = pinnedPkgs; } // pinnedPkgs."${package}")
   ) a;
 in builtins.mapAttrs mapToPackage json
