afe24e784c73813f129e56a73b0314be463d7632
Reed <mrnitsuj1@gmail.com>
2024-02-04 12:40:55 -0500

    ranger: use versionConditionalOverride for kitty tmux patch

--- home/ranger/default.nix
+++ home/ranger/default.nix
@@ -47,16 +47,18 @@ in
 {
 
   home.packages = let
-    ranger = myranger.overrideAttrs (old: {
-    version = "1.9.3";
-    src = pkgs.fetchFromGitHub {
-      owner = "Ethsan";
-      repo = "ranger";
-      rev = "71a06f28551611d192d3e644d95ad04023e10801";
-      sha256 = "sha256-Yjdn1oE5VtJMGnmQ2VC764UXKm1PrkIPXXQ8MzQ8u1U=";
-    };
-    propagatedBuildInputs = old.propagatedBuildInputs ++ (with pkgs.python3Packages; [ astroid pylint ]);
-  });
+    ranger = pkgs.versionConditionalOverride "1.9.3" myranger
+      # https://github.com/ranger/ranger/pull/2856
+      myranger.overrideAttrs (old: {
+        version = "1.9.3";
+        src = pkgs.fetchFromGitHub {
+          owner = "Ethsan";
+          repo = "ranger";
+          rev = "71a06f28551611d192d3e644d95ad04023e10801";
+          sha256 = "sha256-Yjdn1oE5VtJMGnmQ2VC764UXKm1PrkIPXXQ8MzQ8u1U=";
+        };
+        propagatedBuildInputs = old.propagatedBuildInputs ++ (with pkgs.python3Packages; [ astroid pylint ]);
+      });
     myranger = pkgs.ranger.overrideAttrs (_: {
       buildInputs = _.buildInputs ++ [ pkgs.makeWrapper ];
       postInstall = ''
