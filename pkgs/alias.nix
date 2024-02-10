4c702b04eda69ef58f9b9bb15bcec3dde8ca5a7c
Reed <mrnitsuj1@gmail.com>
2024-02-10 11:39:08 -0500

    pkgs: unpin bashmount

--- pkgs/alias.nix
+++ pkgs/alias.nix
@@ -3,11 +3,6 @@ let
   lib = pkgs.lib;
 in
 {
-  bashmount = if self.hasMainProgram pkgs.bashmount
-    then lib.warn "bashmount in nixpkgs has a mainProgram attribute. Remove this override."
-         pkgs.bashmount
-    else pkgs.pinned.bashmount.v4_3_2;
-
   clipit = if self.hasMainProgram pkgs.clipit
     then lib.warn "clipit in nixpkgs has a mainProgram attribute. Remove this override."
          pkgs.clipit
