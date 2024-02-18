9ecf6dd6ad2b5b803a0434813ec4dfbab561a8c0
Reed <mrnitsuj1@gmail.com>
2024-02-18 10:52:16 -0500

    htop: add module

---
+++ home/htop/default.nix
@@ -0,0 +1,14 @@
+{ ... }:
+
+{
+  programs.htop = {
+    enable = true;
+    settings = {
+      tree_view = 1;
+      show_program_path = 0;
+      hide_kernel_threads = 1;
+      hide_userland_threads = 1;
+      highlight_base_name = 1;
+    };
+  };
+}
