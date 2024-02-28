91dbd1ddd2d43a56e2aa25f6a5ab10d0c40619f1
Reed <mrnitsuj1@gmail.com>
2024-02-28 07:44:41 -0500

    pkgs(pin): overlay should warn when using pinned version overlay

--- pkgs/pin/overlay.nix
+++ pkgs/pin/overlay.nix
@@ -10,6 +10,6 @@ let
     # find packages that don't exist in top level
     (lib.filterAttrs (n: v: !lib.hasAttr n pkgs))
     # map pinned to top level
-    (builtins.mapAttrs (n: v: builtins.elemAt (lib.mapAttrsToList (n2: v2: v2) v) 0))
+    (builtins.mapAttrs (n: v: builtins.elemAt (lib.mapAttrsToList (n2: v2: lib.warn "${n} not found. Using pinned version ${n2}." v2) v) 0))
   ];
 in final
