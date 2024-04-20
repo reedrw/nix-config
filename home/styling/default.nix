e4874ea475f3c4ef40dd58c8979c17aa93ace8a6
Reed <mrnitsuj1@gmail.com>
2024-04-20 14:30:55 -0400

    styling: fix qt6 and gtk3 styling

--- home/styling/default.nix
+++ home/styling/default.nix
@@ -8,10 +8,12 @@ with inputs.nix-colors;
   home = {
     packages = with pkgs; [
       adwaita-qt
+      adwaita-qt6
       libsForQt5.qtstyleplugins
       papirus-icon-theme
       qt5.qtbase
       qt5ct
+      qt6ct
     ];
 
     sessionVariables = {
@@ -48,8 +50,8 @@ with inputs.nix-colors;
       style=Adwaita-Dark
 
       [Fonts]
-      fixed=@Variant(\0\0\0@\0\0\0\x14\0S\0\x61\0n\0s\0 \0S\0\x65\0r\0i\0\x66@\"\0\0\0\0\0\0\xff\xff\xff\xff\x5\x1\0\x32\x10)
-      general=@Variant(\0\0\0@\0\0\0\x14\0S\0\x61\0n\0s\0 \0S\0\x65\0r\0i\0\x66@\"\0\0\0\0\0\0\xff\xff\xff\xff\x5\x1\0\x32\x10)
+      fixed="Monospace,9,-1,5,50,0,0,0,0,0"
+      general="Sans Serif,9,-1,5,50,0,0,0,0,0"
 
       [Interface]
       activate_item_on_single_click=1
@@ -68,6 +70,39 @@ with inputs.nix-colors;
 
       [SettingsWindow]
       geometry=@ByteArray(\x1\xd9\xd0\xcb\0\x3\0\0\0\0\0\xc1\0\0\x1\f\0\0\x3\xa9\0\0\x3\xdb\0\0\0\xc6\0\0\x1\x11\0\0\x3\xa4\0\0\x3\xd6\0\0\0\0\0\0\0\0\a\x80\0\0\0\xc6\0\0\x1\x11\0\0\x3\xa4\0\0\x3\xd6)
+    '';
+    "qt6ct/qt6ct.conf".text = ''
+      [Appearance]
+      color_scheme_path=${pkgs.qt6ct}/share/qt5ct/colors/airy.conf
+      custom_palette=false
+      standard_dialogs=default
+      style=Adwaita-Dark
+
+      [Fonts]
+      fixed="Monospace,9,-1,5,400,0,0,0,0,0,0,0,0,0,0,1"
+      general="Sans Serif,9,-1,5,400,0,0,0,0,0,0,0,0,0,0,1"
+
+      [Interface]
+      activate_item_on_single_click=1
+      buttonbox_layout=0
+      cursor_flash_time=1000
+      dialog_buttons_have_icons=1
+      double_click_interval=400
+      gui_effects=@Invalid()
+      keyboard_scheme=2
+      menus_have_icons=true
+      show_shortcuts_in_context_menus=true
+      stylesheets=@Invalid()
+      toolbutton_style=4
+      underline_shortcut=1
+      wheel_scroll_lines=3
+
+      [SettingsWindow]
+      geometry=@ByteArray(\x1\xd9\xd0\xcb\0\x3\0\0\0\0\0\a\0\0\x2-\0\0\x3\xbc\0\0\x4\x30\0\0\0\f\0\0\x2\x32\0\0\x3\xb7\0\0\x4+\0\0\0\0\0\0\0\0\a\x80\0\0\0\f\0\0\x2\x32\0\0\x3\xb7\0\0\x4+)
+
+      [Troubleshooting]
+      force_raster_widgets=1
+      ignored_applications=@Invalid()
     '';
     "gtk-4.0/gtk.css".text = ''
       window {
@@ -100,6 +135,7 @@ with inputs.nix-colors;
     };
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = true;
+      gtk-font-name = "Cantarell 10";
       gtk-xft-antialias = 1;
       gtk-xft-hinting = 1;
       gtk-xft-hintstyle= "hintmedium";
