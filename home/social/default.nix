abebe0b91b3c5fa734ba14bd6c2e1838a1efb8d1
Reed <mrnitsuj1@gmail.com>
2024-05-16 12:39:57 -0400

    social: fix telegram-desktop minimizing instead of closing

--- home/social/default.nix
+++ home/social/default.nix
@@ -34,4 +34,10 @@ in
     "discord/settings.json".source = ./discord-settings.json;
     "discordcanary/settings.json".source = ./discord-settings.json;
   };
+
+  # doesn't do anything on i3, but needed for Telegram to close without minimizing
+  # https://github.com/telegramdesktop/tdesktop/issues/27190#issuecomment-1840780300
+  dconf.settings = {
+    "org/gnome/desktop/wm/preferences".button-layout = ":minimize,maximize,close";
+  };
 }
