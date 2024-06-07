376f7a5aa2012bfcb61d4205863de774a9c0c9b3
Reed <mrnitsuj1@gmail.com>
2024-06-07 02:43:39 -0400

    nixos-desktop: enable ollama

--- system/nixos-desktop/configuration.nix
+++ system/nixos-desktop/configuration.nix
@@ -25,6 +25,11 @@
   boot.initrd.services.lvm.enable = true;
   services.lvm.boot.thin.enable = true;
 
+  services.ollama = {
+    enable = true;
+    acceleration = "rocm";
+  };
+
   # services.foldingathome = {
   #   enable = true;
   #   user = "reed";
