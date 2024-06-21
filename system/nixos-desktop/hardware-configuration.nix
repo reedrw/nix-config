1a5eee8ea7b9db7cbbd2d4393894a5bebd112d80
Reed <mrnitsuj1@gmail.com>
2024-06-21 12:06:44 -0400

    nixos-desktop: enable zram swap

--- system/nixos-desktop/hardware-configuration.nix
+++ system/nixos-desktop/hardware-configuration.nix
@@ -66,6 +66,11 @@
       }
     ];
 
+  zramSwap = {
+    enable = true;
+    memoryPercent = 100;
+  };
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
