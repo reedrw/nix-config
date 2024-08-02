6d4159e07201592ce26c81c2f53a3cf718c45b25
Reed <mrnitsuj1@gmail.com>
2024-08-02 12:11:37 -0400

    comma: use flake input outPath instead of pkgs.path

--- home/comma/command-not-found.nix
+++ home/comma/command-not-found.nix
@@ -1,10 +1,10 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, inputs, lib, ... }:
 let
   dbPath = "/nix/var/nix/profiles/per-user/root/channels/nixos/programs.sqlite";
   commandNotFound = pkgs.substituteAll {
     name = "command-not-found";
     dir = "bin";
-    src = "${pkgs.path}/nixos/modules/programs/command-not-found/command-not-found.pl";
+    src = "${inputs.nixpkgs}/nixos/modules/programs/command-not-found/command-not-found.pl";
     isExecutable = true;
     inherit dbPath;
     perl = pkgs.perl.withPackages (p: [ p.DBDSQLite p.StringShellQuote ]);
