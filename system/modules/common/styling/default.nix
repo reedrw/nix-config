a48af4b9f71ff68968470c60e6fd2ee146efe79e
Reed <mrnitsuj1@gmail.com>
2024-08-14 17:57:12 -0400

    styling: change colorscheme and wallpaper

--- system/modules/common/styling/default.nix
+++ system/modules/common/styling/default.nix
@@ -8,8 +8,8 @@
     enable = true;
     autoEnable = false;
     polarity = "dark";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/horizon-terminal-${config.stylix.polarity}.yaml";
-    image = ./wallpaper.jpg;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-${config.stylix.polarity}.yaml";
+    image = ./wallpaper.png;
   };
 
   stylix.cursor = {
