dbd1dd272e94c7310901e600da90f7f18a771e56
Reed <mrnitsuj1@gmail.com>
2024-08-17 07:42:58 -0400

    pkgs(pinned): pin qbittorrent for mainProgram fix

--- pkgs/alias.nix
+++ pkgs/alias.nix
@@ -7,8 +7,8 @@ in
     screenshotCommand = "${lib.getExe pkgs.maim} -u";
   };
 
-  # https://github.com/NixOS/nixpkgs/pull/317924
-  mods = if pkgs.mods.version != "1.3.1"
-  then lib.warn "mods version changed. PR should be merged." pkgs.pinned.mods.v1_4_0
-  else pkgs.pinned.mods.v1_4_0;
+  # https://github.com/NixOS/nixpkgs/pull/322548
+  qbittorrent-nox = if builtins.hasAttr "mainProgram" pkgs.qbittorrent-nox.meta
+    then lib.warn "qbittorrent-nox mainProgram fixed. PR should be merged." pkgs.pinned.qbittorrent-nox.v4_6_5
+    else pkgs.pinned.qbittorrent-nox.v4_6_5;
 }
