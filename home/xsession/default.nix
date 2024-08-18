ce73d24a7b89594cf693f6ea9bbd358909e40fb7
Reed <mrnitsuj1@gmail.com>
2024-08-18 11:40:24 -0400

    xsession: clean up wallpaper settings a little

--- home/xsession/default.nix
+++ home/xsession/default.nix
@@ -9,9 +9,8 @@ let
     filename = "wallpaper.${fileType}";
     colorScheme = lib.pipe config.lib.stylix.colors [
       (lib.getAttrs [
-        "base00"
-        "base01" "base02" "base03" # "base04"
-        # "base05" "base06" "base07"
+        "base00" "base01" "base02" "base03"
+        # "base04" "base05" "base06" "base07"
         # "base08" "base09" "base0A" "base0B"
         # "base0C" "base0D" "base0E" "base0F"
       ])
