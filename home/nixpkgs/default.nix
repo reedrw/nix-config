5015f1b3cbdb95173086e0ea095d4368dd593e40
Reed <mrnitsuj1@gmail.com>
2024-08-20 09:16:10 -0400

    home/nixpkgs: remove unneeded

--- home/nixpkgs/default.nix
+++ home/nixpkgs/default.nix
@@ -1,22 +1,5 @@
-{ nixConfig, inputs, osConfig, lib, ... }:
+{ ... }:
 
 {
-  home.sessionVariables = {
-    NIX_PATH = "unstable=${inputs.unstable.outPath}:nixpkgs=${inputs.nixpkgs.outPath}$\{NIX_PATH:+:$NIX_PATH}";
-  };
-
-  nix = {
-    package = lib.mkDefault osConfig.nix.package;
-
-    registry = {
-      nixpkgs.flake = inputs.nixpkgs;
-      unstable.flake = inputs.unstable;
-    };
-
-    settings = {
-      inherit (nixConfig) extra-substituters extra-trusted-public-keys;
-      use-xdg-base-directories = true;
-    };
-  };
-
+  nix.settings.use-xdg-base-directories = true;
 }
