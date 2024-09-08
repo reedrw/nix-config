4ecacb184fb1a1da1d2864411ddb9677ab41eb84
Reed <mrnitsuj1@gmail.com>
2024-09-08 10:06:25 -0400

    mpv: mpv_thumbnail_script -> thumbfast

--- home/mpv/default.nix
+++ home/mpv/default.nix
@@ -21,12 +21,7 @@ in
   xdg.configFile = {
     "mpv/scripts/clipshot.lua".source = "${plugins.mpv-scripts}/clipshot.lua";
     "mpv/scripts/webm.lua".source = "${plugins.mpv-webm}/build/webm.lua";
-    "mpv/scripts/mpv_thumbnail_script_client_osc.lua".source = "${plugins.mpv_thumbnail_script}/mpv_thumbnail_script_client_osc.lua";
-    "mpv/scripts/mpv_thumbnail_script_server.lua".source = "${plugins.mpv_thumbnail_script}/mpv_thumbnail_script_server.lua";
-    "mpv/script-opts/mpv_thumbnail_script.conf".text = ''
-      mpv_no_sub=yes
-      cache_directory=${config.home.homeDirectory}/.cache/mpv/my_mpv_thumbnails
-      autogenerate_max_duration=0
-    '';
+    "mpv/scripts/thumbfast.lua".source = "${plugins.thumbfast}/thumbfast.lua";
+    "mpv/scripts/osc.lua".source = "${plugins.vanilla-osc}/player/lua/osc.lua";
   };
 }
