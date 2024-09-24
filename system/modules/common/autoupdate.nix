067b4ad5469cfa9ec2b9a7acbabc4c4004447959
reedrw <mrnitsuj1@gmail.com>
2024-09-24 02:13:01 -0400

    autoupdate: just add 100 second delay and assume it works

--- system/modules/common/autoupdate.nix
+++ system/modules/common/autoupdate.nix
@@ -5,6 +5,7 @@ let
     runtimeInputs = with pkgs; [ git ldp coreutils ];
     text = ''
       set -x
+      sleep 100
       export PATH="/run/wrappers/bin:$PATH"
       export PATH="/run/current-system/sw/bin:$PATH"
       git(){
