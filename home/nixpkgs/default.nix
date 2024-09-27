03108e48edc816bb773e9b3435c455ed9ab26b59
Reed <mrnitsuj1@gmail.com>
2024-09-26 22:14:40 -0400

    pkgs(gc): gc -D should check HM generations as well

--- home/nixpkgs/default.nix
+++ home/nixpkgs/default.nix
@@ -1,8 +1,10 @@
-{ osConfig, lib, ... }:
+{ osConfig, lib, versionSuffix, ... }:
 
 {
   nix = {
     package = lib.mkDefault osConfig.nix.package;
     settings.use-xdg-base-directories = true;
   };
+
+  xdg.dataFile."home-manager/tree-version".text = versionSuffix;
 }
