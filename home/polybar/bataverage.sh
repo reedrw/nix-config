2d2737890ce1ec4633790bcfbac5bf02b92b8a7c
reedrw <mrnitsuj1@gmail.com>
2024-09-28 13:43:54 -0400

    polybar(bataverage): try to exclude batteries from external peripherals

--- home/polybar/bataverage.sh
+++ home/polybar/bataverage.sh
@@ -3,7 +3,10 @@
 # Gets the average battery percentage of all batteries on the system.
 
 if command -v acpi &> /dev/null; then
-  acpi | sed -e 's/%.*$//g' -e 's/^.*, //g' | awk '{ sum += $1; n++ } END { if (n > 0) printf "%d%\n", int(sum / n); }'
+  acpi \
+    | grep -v "rate information unavailable" \
+    | sed -e 's/%.*$//g' -e 's/^.*, //g' \
+    | awk '{ sum += $1; n++ } END { if (n > 0) printf "%d%\n", int(sum / n); }'
 else sleep infinity
 fi
 sleep 10
