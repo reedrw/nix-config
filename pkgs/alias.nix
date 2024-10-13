e8496f5d95741229cbbf32300c36cf3fc936cde8
Reed <mrnitsuj1@gmail.com>
2024-10-13 10:41:21 -0400

    pkgs(pin): qbittorrent-nox 4.6.5 -> 5.0.0

--- pkgs/alias.nix
+++ pkgs/alias.nix
@@ -14,6 +14,6 @@ in
 
   # https://github.com/NixOS/nixpkgs/pull/322548
   qbittorrent-nox = if builtins.hasAttr "mainProgram" pkgs.qbittorrent-nox.meta
-    then lib.warn "qbittorrent-nox mainProgram fixed. PR should be merged." pkgs.pinned.qbittorrent-nox.v4_6_5
-    else pkgs.pinned.qbittorrent-nox.v4_6_5;
+    then lib.warn "qbittorrent-nox mainProgram fixed. PR should be merged." pkgs.pinned.qbittorrent-nox.v5_0_0
+    else pkgs.pinned.qbittorrent-nox.v5_0_0;
 }
