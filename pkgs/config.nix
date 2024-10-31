57b3672af82dccf4ec0edcc9fa53bf799b0535d3
reedrw <mrnitsuj1@gmail.com>
2024-10-31 19:48:12 -0400

    pkgs(config): remove packageOverrides

--- pkgs/config.nix
+++ pkgs/config.nix
@@ -2,9 +2,4 @@
 
 {
   allowUnfree = true;
-  packageOverrides = pkgs: {}
-    # TODO: figure out a way to apply version overrides to legacy nix
-    # tools like nix-shell
-    # // import ./pin/overlay.nix pkgs pkgs
-    // import ./branches.nix inputs pkgs pkgs;
 }
