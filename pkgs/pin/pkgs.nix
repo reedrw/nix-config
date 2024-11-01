42a14bbd7c647c76ac8ad8b896f6e05e4aaef4f2
Reed <mrnitsuj1@gmail.com>
2024-11-01 01:50:25 -0400

    pkgs(pin): add --note flag

--- pkgs/pin/pkgs.nix
+++ pkgs/pin/pkgs.nix
@@ -1,7 +1,7 @@
 pkgs: let
   lib = pkgs.lib;
   json = lib.importJSON ./pinned.json;
-  pinnedVersionToPkg = { rev, sha256 }: import (builtins.fetchTarball {
+  pinnedVersionToPkg = { rev, sha256, ... }: import (builtins.fetchTarball {
     inherit sha256;
     url = "https://github.com/nixos/nixpkgs/archive/${rev}.tar.gz";
   }) { inherit (pkgs) system config; };
