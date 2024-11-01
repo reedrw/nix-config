5c223e6ac93772d815a9b8c2e74fb184b94d64c6
Reed <mrnitsuj1@gmail.com>
2024-11-01 09:54:26 -0400

    pkgs(pin/overlay): only map defaults to top level

--- pkgs/pin/overlay.nix
+++ pkgs/pin/overlay.nix
@@ -2,16 +2,16 @@ self: pkgs:
 let
   lib = pkgs.lib;
   pinned = pkgs.pinned;
-  # map pinned to top level if top level
-  # is not already defined
+
+  # map defaults to top level
   defaults = pinned
     |> lib.filterAttrs (n: v: lib.hasAttr "default" v)
     |> lib.mapAttrs (n: v: v.default);
 
-  final = (pinned
-    |> lib.mapAttrs (n: v: lib.filterAttrs (n2: v2: n2 != "default") v)
-    |> lib.filterAttrs (n: v: (lib.length (builtins.attrNames v)) == 1)
-    |> lib.filterAttrs (n: v: !lib.hasAttr n pkgs)
-    |> builtins.mapAttrs (n: v: builtins.elemAt (builtins.attrValues v) 0)
-  ) // defaults;
-in final
+  # final = (pinned
+  #   |> lib.mapAttrs (n: v: lib.filterAttrs (n2: v2: n2 != "default") v)
+  #   |> lib.filterAttrs (n: v: (lib.length (builtins.attrNames v)) == 1)
+  #   |> lib.filterAttrs (n: v: !lib.hasAttr n pkgs)
+  #   |> builtins.mapAttrs (n: v: builtins.elemAt (builtins.attrValues v) 0)
+  # ) // defaults;
+in defaults
