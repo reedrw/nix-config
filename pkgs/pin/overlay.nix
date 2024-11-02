f6cc063e4d15bd3dc17e429ea7e02b89717c59d3
Reed <mrnitsuj1@gmail.com>
2024-11-02 10:42:55 -0400

    pkgs(pin): cleanup

--- pkgs/pin/overlay.nix
+++ pkgs/pin/overlay.nix
@@ -7,11 +7,4 @@ let
   defaults = pinned
     |> lib.filterAttrs (n: v: lib.hasAttr "default" v)
     |> lib.mapAttrs (n: v: v.default);
-
-  # final = (pinned
-  #   |> lib.mapAttrs (n: v: lib.filterAttrs (n2: v2: n2 != "default") v)
-  #   |> lib.filterAttrs (n: v: (lib.length (builtins.attrNames v)) == 1)
-  #   |> lib.filterAttrs (n: v: !lib.hasAttr n pkgs)
-  #   |> builtins.mapAttrs (n: v: builtins.elemAt (builtins.attrValues v) 0)
-  # ) // defaults;
 in defaults
