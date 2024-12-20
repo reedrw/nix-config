97e99a9034af554e10f5e222f4ad2aaee31034b2
Reed <mrnitsuj1@gmail.com>
2024-12-20 10:26:39 -0500

    nixos-iso: cleanup

--- nixos-configurations/nixos-iso/configuration.nix
+++ nixos-configurations/nixos-iso/configuration.nix
@@ -1,16 +1,19 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
+{ lib, ... }:
 
 {
-  # imports = [
-  #   ./hardware-configuration.nix
-  # ];
+
+  boot = {
+    plymouth.enable = lib.mkForce false;
+    initrd.systemd.enable = lib.mkForce false;
+  };
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
   networking.hostName = "nixos-iso";
+
   users.users.reed.password = "password";
 
   time.timeZone = "America/New_York";
 
-  system.stateVersion = "22.11";
+  system.stateVersion = lib.trivial.release;
 }
--- nixos-configurations/nixos-iso/default.nix
+++ nixos-configurations/nixos-iso/default.nix
@@ -9,14 +9,6 @@
     ezModules'.networking.networking
     "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/channel.nix"
     "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-base.nix"
-    ({lib, pkgs, ...}: {
-      boot.plymouth.enable = lib.mkForce false;
-      boot.initrd.systemd.enable = lib.mkForce false;
-      nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-      environment.systemPackages = with pkgs; [
-        arandr
-      ];
-    })
     ./configuration.nix
   ];
 }
