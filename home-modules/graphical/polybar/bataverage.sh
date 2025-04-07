e848c7bc56c9350d3cd1d3d78dadd1cc678d3b45
reedrw <mrnitsuj1@gmail.com>
2025-04-07 15:59:19 -0400

    polybar: add + to battery percentage when charging

--- home-modules/graphical/polybar/bataverage.sh
+++ home-modules/graphical/polybar/bataverage.sh
@@ -3,10 +3,17 @@
 # Gets the average battery percentage of all batteries on the system.
 
 if command -v acpi &> /dev/null; then
-  acpi \
+  charging="$(acpi -a | grep -c "on-line")"
+  percentage="$(acpi \
     | grep -v "rate information unavailable" \
     | sed -e 's/%.*$//g' -e 's/^.*, //g' \
     | awk '{ sum += $1; n++ } END { if (n > 0) printf "%d%\n", int(sum / n); }'
+  )"
+  if [ "$charging" -gt 0 ]; then
+    echo "$percentage+"
+  else
+    echo "$percentage"
+  fi
 else sleep infinity
 fi
 sleep 10
