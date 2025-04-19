8ed7f9456a456d2e8e984409732ed4fb9bf8615a
reedrw <mrnitsuj1@gmail.com>
2025-04-19 01:38:17 -0400

    reed@nixos-t480: don't use entire games module

--- home-configurations/reed@nixos-t480.nix
+++ home-configurations/reed@nixos-t480.nix
@@ -1,14 +1,15 @@
-{ ezModules, ... }:
+{ ezModules, ezModules', ... }:
 
 {
   imports = [
     ./reed.nix
     ezModules.core
     ezModules.extra
-    ezModules.games
     ezModules.graphical
     ezModules.media
     ezModules.social
+    ezModules'.games.minecraft
+    ezModules'.games.steam
     ezModules."reed@nixos-t480"
   ];
 }
