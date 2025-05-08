5710e0c6180cc79aaf0bfd658b62bd5c7fe4ee41
Reed <mrnitsuj1@gmail.com>
2025-05-08 10:51:07 -0400

    flake: turn update-all unto proper package output

---
+++ pkgs/update-all/default.nix
@@ -0,0 +1,17 @@
+{ writeShellScriptBin, writeShellScript }:
+
+let
+  blue = ''"$(tput setaf 4)"'';
+  green = ''"$(tput setaf 2)"'';
+  reset = ''"$(tput sgr0)"'';
+  dots = "${blue}....................................................................${reset}";
+
+  echoAndRun = writeShellScript "echoAndRun" ''
+    echo -e "${green}Running $@\n${dots}"
+    $@
+    echo
+  '';
+in writeShellScriptBin "update-all" ''
+  ${echoAndRun} nix flake update
+  find . -name update-sources.sh -execdir sh -c '${echoAndRun} "$(realpath {})"&& echo' \;
+''
