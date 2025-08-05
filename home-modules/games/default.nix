79226e82702406c6a13f06f5fe500d6c8a143167
Reed <mrnitsuj1@gmail.com>
2025-08-05 09:56:51 -0400

    home(games): move bottles into new module

--- home-modules/games/default.nix
+++ home-modules/games/default.nix
@@ -1,10 +1,6 @@
-{ pkgs, ... }:
-
 {
   imports = builtins.readDir ./.
     |> (x: builtins.removeAttrs x ["default.nix"])
     |> builtins.attrNames
     |> map (x: "${./.}/${x}");
-
-  home.packages = [ pkgs.bottles ];
 }
