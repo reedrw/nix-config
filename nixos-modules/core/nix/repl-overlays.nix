b4a532beb0a79ceff9e5874b629d9ac1d9051a9f
Reed <mrnitsuj1@gmail.com>
2025-09-09 21:29:14 -0400

    nix: add pkgs-unstable to repl-overlays

--- nixos-modules/core/nix/repl-overlays.nix
+++ nixos-modules/core/nix/repl-overlays.nix
@@ -3,5 +3,6 @@ info: final: prev:
 {
   self = builtins.getFlake "self";
   pkgs = final.self.nixosConfigurations.${prev.extraInfo.hostName}.pkgs;
+  pkgs-unstable = final.pkgs.pkgs-unstable;
   lib = final.pkgs.lib;
 }
