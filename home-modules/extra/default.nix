c09fcbbf0a0e15cb5c4e34ce61ba3ca0bc1b8e7e
Reed <mrnitsuj1@gmail.com>
2025-09-15 12:56:47 -0400

    home-modules: automatically remove mineapps.list

--- home-modules/extra/default.nix
+++ home-modules/extra/default.nix
@@ -1,6 +1,22 @@
+{ pkgs, config, ... }:
+
 {
   imports = builtins.readDir ./.
     |> (x: builtins.removeAttrs x ["default.nix"])
     |> builtins.attrNames
     |> map (x: "${./.}/${x}");
+
+  # remove ~/.config/mimeapps.list on activation
+  home.activation = let
+    removeMineapps = pkgs.writeShellScript "removeMineapps" ''
+      mimeapps="$HOME/.config/mimeapps.list"
+      if [ -f "$mimeapps" ]; then
+        rm "$mimeapps"
+      fi
+    '';
+  in {
+    removeMineapps = config.lib.dag.entryBefore [ "checkLinkTargets" ] ''
+      run ${removeMineapps}
+    '';
+  };
 }
