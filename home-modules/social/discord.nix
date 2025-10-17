0ca7ff6851f22a7fad6d9f6ff70dcb5081d96361
Reed <mrnitsuj1@gmail.com>
2025-10-17 12:41:11 -0400

    discord: alphabetize plugins

--- home-modules/social/discord.nix
+++ home-modules/social/discord.nix
@@ -12,33 +12,36 @@ in
     discord = {
       autoscroll.enable = true;
     };
-    config.plugins = {
-      youtubeAdblock.enable = true;
-      betterUploadButton.enable = true;
-      clearURLs.enable = true;
-      fakeNitro.enable = true;
-      iLoveSpam.enable = true;
-      loadingQuotes.enable = true;
-      messageLinkEmbeds.enable = true;
-      noBlockedMessages.enable = true;
-      normalizeMessageLinks.enable = true;
-      silentTyping = {
-        enable = true;
-        showIcon = true;
-        contextMenu = true;
-        isEnabled = false;
-      };
-      translate.enable = true;
-      typingIndicator.enable = true;
-      unindent.enable = true;
-      voiceMessages.enable = true;
-      fixImagesQuality.enable = true;
-      fixYoutubeEmbeds.enable = true;
-      betterSettings.enable = true;
-      replaceGoogleSearch = {
-        enable = true;
-        customEngineName = "DuckDuckGo";
-        customEngineURL = "https://duckduckgo.com/";
+    config = {
+      frameless = true;
+      plugins = {
+        betterSettings.enable = true;
+        betterUploadButton.enable = true;
+        clearURLs.enable = true;
+        fakeNitro.enable = true;
+        fixImagesQuality.enable = true;
+        fixYoutubeEmbeds.enable = true;
+        iLoveSpam.enable = true;
+        loadingQuotes.enable = true;
+        messageLinkEmbeds.enable = true;
+        noBlockedMessages.enable = true;
+        normalizeMessageLinks.enable = true;
+        replaceGoogleSearch = {
+          enable = true;
+          customEngineName = "DuckDuckGo";
+          customEngineURL = "https://duckduckgo.com/";
+        };
+        silentTyping = {
+          enable = true;
+          contextMenu = true;
+          isEnabled = false;
+          showIcon = true;
+        };
+        translate.enable = true;
+        typingIndicator.enable = true;
+        unindent.enable = true;
+        voiceMessages.enable = true;
+        youtubeAdblock.enable = true;
       };
     };
   };
