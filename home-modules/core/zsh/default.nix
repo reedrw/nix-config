5400be3fbab9c26a982838ed2ef85e3d80b0d4a0
Reed <mrnitsuj1@gmail.com>
2025-11-10 22:21:59 -0500

    treewide: remove lorri in favor of direnv-instant

--- home-modules/core/zsh/default.nix
+++ home-modules/core/zsh/default.nix
@@ -3,9 +3,12 @@ let
   sources = (util.importFlake ./plugins).inputs or {};
 in
 {
+  imports = [
+    sources.direnv-instant.homeModules.direnv-instant
+  ];
+
   programs.direnv = {
     enable = true;
-    enableZshIntegration = true;
     nix-direnv = {
       enable = true;
     };
@@ -13,6 +16,11 @@ in
       hide_env_diff = true;
       load_dotenv = true;
     };
+  };
+
+  programs.direnv-instant = {
+    enable = true;
+    package = sources.direnv-instant.packages."${pkgs.system}".default;
   };
 
   stylix.targets.fzf.enable = true;
--- home-modules/core/zsh/plugins/flake.nix
+++ home-modules/core/zsh/plugins/flake.nix
@@ -4,6 +4,9 @@
       url = "github:DeveloperC286/zsh-simple-abbreviations";
       flake = false;
     };
+    direnv-instant = {
+      url = "github:Mic92/direnv-instant";
+    };
   };
   outputs = _: { };
 }
