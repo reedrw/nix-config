5b5098b53bcfb3a18ccb35a0565b0536c97d9268
Reed <mrnitsuj1@gmail.com>
2025-11-28 11:21:20 -0500

    flake: use home-manager 25.11

--- flake.nix
+++ flake.nix
@@ -49,8 +49,7 @@ let self = {
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager";
-      # url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
