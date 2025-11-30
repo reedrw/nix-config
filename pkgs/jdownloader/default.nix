23c2b081a7db51c462fecf58d7938b8a4ee44734
Reed <mrnitsuj1@gmail.com>
2025-11-30 18:09:32 -0500

    pkgs(jdownloader): add .desktop file and icon

--- pkgs/jdownloader/default.nix
+++ pkgs/jdownloader/default.nix
@@ -1,144 +1,53 @@
 {
-  stdenv,
-  writeShellScript,
-  writeShellApplication,
-  coreutils,
-  fetchFromGitHub,
-  jq,
+  symlinkJoin,
+  makeDesktopItem,
+  graphicsmagick,
+  fetchurl,
+  callPackage,
   lib,
-  megacmd,
-  openjdk,
+
   darkTheme ? false,
   debloat ? true,
   extraOptions ? {}
 }:
 
 let
-  fetchFromMegaNz = { name, url, sha256, isDir ? false }:
-  # isDir: mega.nz allows to download folders
-  # with isDir == false, the remote filename is ignored
-  # for single files, isDir == true will produce a different sha256 than isDir == false
-  stdenv.mkDerivation {
-    inherit name;
-    outputHashMode = "recursive";
-    outputHashAlgo = "sha256";
-    outputHash = sha256;
-    builder = writeShellScript "builder.sh" ''
-      PATH=${lib.makeBinPath [ megacmd coreutils ]}
-
-      # workaround for https://github.com/meganz/MEGAcmd/issues/580
-      export HOME=/tmp/home
-      mkdir -p $HOME
-
-      mkdir tmpdir
-      cd tmpdir
-      mega-get ${url}
-
-      ${
-        if isDir
-        then ''
-          mkdir $out
-          mv * $out
-        ''
-        else ''
-          if [ $(ls -A | wc -l) -ne 1 ]; then
-            echo "error: isDir is false, but download produced multiple files:"
-            ls -A
-            exit 1
-          fi
-          mkdir $out
-          cd ..
-          mv tmpdir $out
-        ''
-      }
-    '';
-    buildInputs = [ megacmd coreutils ];
+  icon = fetchurl {
+    url = "https://jdownloader.org/_media/vote/trazo.png";
+    sha256 = "3ebab992e7dd04ffcb6c30fee1a7e2b43f3537cb2b22124b30325d25bffdac29";
   };
 
-  src = fetchFromMegaNz {
-    name= "JDownloader";
-    url = "https://mega.nz/file/3dkXRLTb#tBmI9K0jrbjkh57CArqkVPbmMZ8l11JypDbM8RncqdU";
-    sha256="sha256-stMfQwRhng+apYtUaDx7eavP4Zmb3cucISIviVButtQ=";
-  };
-
-  darkThemeSrc = fetchFromGitHub {
-    owner = "moktavizen";
-    repo = "material-darker-jdownloader";
-    rev = "f004ca290e903a6732c3be9955b5acaef582a268";
-    hash = "sha256-pct3O5jV2X2/Uc17DKICgxh4AMfTMrLv7pTgh9xxRsM=";
-  };
-
-  darkThemeOptions = {
-    "org.jdownloader.settings.GraphicalUserInterfaceSettings" = {
-      lookandfeeltheme = "FLATLAF_DARK";
-    };
-  };
-
-  # Apply debloating settings from:
-  # https://claraiscute.neocities.org/Guides/jdownloader2/
-  debloatOptions = {
-    "org.jdownloader.settings.GraphicalUserInterfaceSettings" = {
-      bannerenabled = false;
-      donatebuttonstate = "AUTO_HIDDEN";
-      premiumalertetacolumnenabled = false;
-      premiumalertspeedcolumnenabled = false;
-      premiumalerttaskcolumnenabled = false;
-      specialdealoboomdialogvisibleonstartup = false;
-      specialdealsenabled = false;
-      speedmetervisible = false;
-    };
-  };
-
-  finalOptions = lib.foldl' (a: x: lib.recursiveUpdate a x) {} [
-    (lib.optionalAttrs darkTheme darkThemeOptions)
-    (lib.optionalAttrs debloat debloatOptions)
-    extraOptions
-  ];
-in writeShellApplication {
+  wrapper = callPackage ./wrapper.nix { inherit darkTheme debloat extraOptions; };
+in symlinkJoin {
   name = "jdownloader";
-  runtimeInputs = [ openjdk jq ];
-  text = ''
-    if [ ! -d "$XDG_DATA_HOME/jdownloader" ]; then
-      mkdir "$XDG_DATA_HOME/jdownloader"
-    fi
 
-    if [ ! -f "$XDG_DATA_HOME/jdownloader/JDownloader.jar" ]; then
-      install -m 644 ${src}/JDownloader.jar "$XDG_DATA_HOME/jdownloader"
-    fi
+  paths = [
+    wrapper
+    (makeDesktopItem {
+      name = "JDownloader 2";
+      exec = "${wrapper}/bin/jdownloader";
+      icon = "jdownloader";
+      comment = "Free, open-source download management tool.";
+      desktopName = "JDownloader 2";
+      genericName = "JDownloader 2";
+      categories = ["Network"];
+    })
+  ];
 
-    if [ ! -d "$XDG_DATA_HOME/jdownloader/cfg" ]; then
-      mkdir "$XDG_DATA_HOME/jdownloader/cfg"
-    fi
-  '' + lib.concatStringsSep "\n" (lib.mapAttrsToList (n: v: ''
-    if [ -f "$XDG_DATA_HOME/jdownloader/cfg/${n}.json" ]; then
-      tmp="$(jq -r '${lib.concatStringsSep "|" (lib.mapAttrsToList (n1: v1: ".${n1}=${builtins.toJSON v1}") v)}' \
-        "$XDG_DATA_HOME/jdownloader/cfg/${n}.json"
-      )" && cat <<< "$tmp" > "$XDG_DATA_HOME/jdownloader/cfg/${n}.json"
-    else
-      echo '${builtins.toJSON v}' > "$XDG_DATA_HOME/jdownloader/cfg/${n}.json"
-    fi
-  '' ) finalOptions) + lib.optionalString darkTheme ''
-    mkdir -p "$XDG_DATA_HOME/jdownloader/themes/standard/org/jdownloader"
-    mkdir -p "$XDG_DATA_HOME/jdownloader/libs/laf"
+  postBuild = ''
+    mkdir -pv $out/bin $out/share/applications
 
-    if [ -d "$XDG_DATA_HOME/jdownloader/themes/standard/org/jdownloader/images" ]; then
-      rm -r "$XDG_DATA_HOME/jdownloader/themes/standard/org/jdownloader/images"
-      cp -r "${darkThemeSrc}/images" "$XDG_DATA_HOME/jdownloader/themes/standard/org/jdownloader"
-      find "$XDG_DATA_HOME/jdownloader/themes/standard/org/jdownloader" -exec chmod u+w {} \;
-    fi
-
-    if [ ! -d "$XDG_DATA_HOME/jdownloader/cfg/laf" ]; then
-      cp -r "${darkThemeSrc}/laf" "$XDG_DATA_HOME/jdownloader/cfg"
-      find "$XDG_DATA_HOME/jdownloader/cfg" -exec chmod u+w {} \;
-    fi
-
-    if [ -f "$XDG_DATA_HOME/jdownloader/libs/laf/flatlaf.jar" ]; then
-      rm "$XDG_DATA_HOME/jdownloader/libs/laf/flatlaf.jar"
-      install "${darkThemeSrc}/flatlaf.jar" "$XDG_DATA_HOME/jdownloader/libs/laf"
-    fi
-
-  '' + ''
-    java -jar "$XDG_DATA_HOME/jdownloader/JDownloader.jar"
+    for size in 16 24 32 48 64 128 256 512; do
+      mkdir -p $out/share/icons/hicolor/"$size"x"$size"/apps
+      ${lib.getExe graphicsmagick} convert -resize "$size"x"$size" ${icon} $out/share/icons/hicolor/"$size"x"$size"/apps/jdownloader.png
+    done
   '';
-  meta.mainProgram = "jdownloader";
+
+  meta = with lib; {
+    homepage = "https://jdownloader.org/";
+    description = "Free, open-source download management tool";
+    license = licenses.gpl3Plus;
+    platforms = platforms.unix;
+    mainProgram = "jdownloader";
+  };
 }
---
+++ pkgs/jdownloader/wrapper.nix
@@ -0,0 +1,143 @@
+{
+  stdenv,
+  writeShellScript,
+  writeShellApplication,
+  coreutils,
+  fetchFromGitHub,
+  jq,
+  lib,
+  megacmd,
+  jre,
+  darkTheme ? false,
+  debloat ? true,
+  extraOptions ? {}
+}:
+
+let
+  fetchFromMegaNz = { name, url, sha256, isDir ? false }:
+  # isDir: mega.nz allows to download folders
+  # with isDir == false, the remote filename is ignored
+  # for single files, isDir == true will produce a different sha256 than isDir == false
+  stdenv.mkDerivation {
+    inherit name;
+    outputHashMode = "recursive";
+    outputHashAlgo = "sha256";
+    outputHash = sha256;
+    builder = writeShellScript "builder.sh" ''
+      PATH=${lib.makeBinPath [ megacmd coreutils ]}
+
+      # workaround for https://github.com/meganz/MEGAcmd/issues/580
+      export HOME=/tmp/home
+      mkdir -p $HOME
+
+      mkdir tmpdir
+      cd tmpdir
+      mega-get ${url}
+
+      ${
+        if isDir
+        then ''
+          mkdir $out
+          mv * $out
+        ''
+        else ''
+          if [ $(ls -A | wc -l) -ne 1 ]; then
+            echo "error: isDir is false, but download produced multiple files:"
+            ls -A
+            exit 1
+          fi
+          mkdir $out
+          cd ..
+          mv tmpdir $out
+        ''
+      }
+    '';
+    buildInputs = [ megacmd coreutils ];
+  };
+
+  src = fetchFromMegaNz {
+    name= "JDownloader";
+    url = "https://mega.nz/file/3dkXRLTb#tBmI9K0jrbjkh57CArqkVPbmMZ8l11JypDbM8RncqdU";
+    sha256="sha256-stMfQwRhng+apYtUaDx7eavP4Zmb3cucISIviVButtQ=";
+  };
+
+  darkThemeSrc = fetchFromGitHub {
+    owner = "moktavizen";
+    repo = "material-darker-jdownloader";
+    rev = "f004ca290e903a6732c3be9955b5acaef582a268";
+    hash = "sha256-pct3O5jV2X2/Uc17DKICgxh4AMfTMrLv7pTgh9xxRsM=";
+  };
+
+  darkThemeOptions = {
+    "org.jdownloader.settings.GraphicalUserInterfaceSettings" = {
+      lookandfeeltheme = "FLATLAF_DARK";
+    };
+  };
+
+  # Apply debloating settings from:
+  # https://claraiscute.neocities.org/Guides/jdownloader2/
+  debloatOptions = {
+    "org.jdownloader.settings.GraphicalUserInterfaceSettings" = {
+      bannerenabled = false;
+      donatebuttonstate = "AUTO_HIDDEN";
+      premiumalertetacolumnenabled = false;
+      premiumalertspeedcolumnenabled = false;
+      premiumalerttaskcolumnenabled = false;
+      specialdealoboomdialogvisibleonstartup = false;
+      specialdealsenabled = false;
+      speedmetervisible = false;
+    };
+  };
+
+  finalOptions = lib.foldl' (a: x: lib.recursiveUpdate a x) {} [
+    (lib.optionalAttrs darkTheme darkThemeOptions)
+    (lib.optionalAttrs debloat debloatOptions)
+    extraOptions
+  ];
+in writeShellApplication {
+  name = "jdownloader";
+  runtimeInputs = [ jre jq ];
+  text = ''
+    if [ ! -d "$XDG_DATA_HOME/jdownloader" ]; then
+      mkdir "$XDG_DATA_HOME/jdownloader"
+    fi
+
+    if [ ! -f "$XDG_DATA_HOME/jdownloader/JDownloader.jar" ]; then
+      install -m 644 ${src}/JDownloader.jar "$XDG_DATA_HOME/jdownloader"
+    fi
+
+    if [ ! -d "$XDG_DATA_HOME/jdownloader/cfg" ]; then
+      mkdir "$XDG_DATA_HOME/jdownloader/cfg"
+    fi
+  '' + lib.concatStringsSep "\n" (lib.mapAttrsToList (n: v: ''
+    if [ -f "$XDG_DATA_HOME/jdownloader/cfg/${n}.json" ]; then
+      tmp="$(jq -r '${lib.concatStringsSep "|" (lib.mapAttrsToList (n1: v1: ".${n1}=${builtins.toJSON v1}") v)}' \
+        "$XDG_DATA_HOME/jdownloader/cfg/${n}.json"
+      )" && cat <<< "$tmp" > "$XDG_DATA_HOME/jdownloader/cfg/${n}.json"
+    else
+      echo '${builtins.toJSON v}' > "$XDG_DATA_HOME/jdownloader/cfg/${n}.json"
+    fi
+  '' ) finalOptions) + lib.optionalString darkTheme ''
+    mkdir -p "$XDG_DATA_HOME/jdownloader/themes/standard/org/jdownloader"
+    mkdir -p "$XDG_DATA_HOME/jdownloader/libs/laf"
+
+    if [ -d "$XDG_DATA_HOME/jdownloader/themes/standard/org/jdownloader/images" ]; then
+      rm -r "$XDG_DATA_HOME/jdownloader/themes/standard/org/jdownloader/images"
+      cp -r "${darkThemeSrc}/images" "$XDG_DATA_HOME/jdownloader/themes/standard/org/jdownloader"
+      find "$XDG_DATA_HOME/jdownloader/themes/standard/org/jdownloader" -exec chmod u+w {} \;
+    fi
+
+    if [ ! -d "$XDG_DATA_HOME/jdownloader/cfg/laf" ]; then
+      cp -r "${darkThemeSrc}/laf" "$XDG_DATA_HOME/jdownloader/cfg"
+      find "$XDG_DATA_HOME/jdownloader/cfg" -exec chmod u+w {} \;
+    fi
+
+    if [ -f "$XDG_DATA_HOME/jdownloader/libs/laf/flatlaf.jar" ]; then
+      rm "$XDG_DATA_HOME/jdownloader/libs/laf/flatlaf.jar"
+      install "${darkThemeSrc}/flatlaf.jar" "$XDG_DATA_HOME/jdownloader/libs/laf"
+    fi
+
+  '' + ''
+    java -jar "$XDG_DATA_HOME/jdownloader/JDownloader.jar"
+  '';
+}
