785a9e29977c8696d4f81168ee057080a3ec5487
Reed <mrnitsuj1@gmail.com>
2025-12-04 10:42:59 -0500

    home(extra): simpler mimeapps override solution

--- home-modules/extra/default.nix
+++ home-modules/extra/default.nix
@@ -1,5 +1,3 @@
-{ pkgs, config, ... }:
-
 {
   imports = builtins.readDir ./.
     |> (x: builtins.removeAttrs x ["default.nix"])
@@ -7,16 +5,5 @@
     |> map (x: ./${x});
 
   # remove ~/.config/mimeapps.list on activation
-  home.activation = let
-    removeMineapps = pkgs.writeShellScript "removeMineapps" ''
-      mimeapps="$HOME/.config/mimeapps.list"
-      if [ -f "$mimeapps" ]; then
-        rm "$mimeapps"
-      fi
-    '';
-  in {
-    removeMineapps = config.lib.dag.entryBefore [ "checkLinkTargets" ] ''
-      run ${removeMineapps}
-    '';
-  };
+  xdg.configFile."mimeapps.list".force = true;
 }
