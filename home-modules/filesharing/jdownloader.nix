1df46ec280373f60269be71d3eec989ab729e521
Reed <mrnitsuj1@gmail.com>
2025-12-10 11:14:37 -0500

    home-modules(filesharing/jdownloader): support light mode lookandfeel

--- home-modules/filesharing/jdownloader.nix
+++ home-modules/filesharing/jdownloader.nix
@@ -3,7 +3,7 @@
 {
   home.packages = with pkgs; [
     (mullvadExclude <| jdownloader.override {
-      darkTheme = true;
+      darkTheme = config.stylix.polarity == "dark";
       extraOptions = {
         "org.jdownloader.extensions.extraction.ExtractionExtension" = {
           deletearchivefilesafterextractionaction = "NULL";
@@ -11,6 +11,10 @@
         "org.jdownloader.settings.GeneralSettings" = {
           maxsimultanedownloads = 5;
           defaultdownloadfolder = "${config.xdg.dataHome}/jdownloader/downloads";
+        };
+        "org.jdownloader.settings.GraphicalUserInterfaceSettings" = lib.optionalAttrs (config.stylix.polarity == "light")
+        {
+          lookandfeeltheme = "FLATLAF_LIGHT";
         };
       };
     })
