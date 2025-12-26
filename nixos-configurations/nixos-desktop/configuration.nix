f30db8affd58249c22dd998bb4fc6ae69c936c5e
Reed <mrnitsuj1@gmail.com>
2025-12-26 12:27:45 -0500

    nixos-configurations(nixos-desktop): change udev rules for io sched

--- nixos-configurations/nixos-desktop/configuration.nix
+++ nixos-configurations/nixos-desktop/configuration.nix
@@ -17,6 +17,17 @@
     8181
   ];
 
+  services.udev.extraRules = ''
+    # HDD
+    ACTION=="add|change", KERNEL=="sd[a-z]*", ATTR{queue/rotational}=="1", ATTR{queue/scheduler}="bfq"
+
+    # SSD
+    ACTION=="add|change", KERNEL=="sd[a-z]*|mmcblk[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="mq-deadline"
+
+    # NVMe SSD
+    ACTION=="add|change", KERNEL=="nvme[0-9]*", ATTR{queue/rotational}=="0", ATTR{queue/scheduler}="none"
+  '';
+
   services.ollama = {
     enable = true;
     acceleration = "rocm";
