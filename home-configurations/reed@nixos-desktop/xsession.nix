f7b11d369de6ef0901259a900925d660a431d8f8
Reed <mrnitsuj1@gmail.com>
2026-02-15 13:00:17 -0500

    home-configurations(reed@nixos-desktop): automatically redirect line-in to easyeffects

--- home-configurations/reed@nixos-desktop/xsession.nix
+++ home-configurations/reed@nixos-desktop/xsession.nix
@@ -1,8 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   xsession.windowManager.i3.config.startup = let
-    alwaysRun = [];
+    alwaysRun = [
+      "${pkgs.writeShellScript "line-in-to-easyeffects" ''
+        sleep 10
+        pw-link "alsa_input.pci-0000_18_00.6.analog-stereo:capture_FL" "easyeffects_sink:playback_FL"
+        pw-link "alsa_input.pci-0000_18_00.6.analog-stereo:capture_FR" "easyeffects_sink:playback_FR"
+      ''}"
+    ];
     run = [
       "xrandr --output DisplayPort-0 --mode 1920x1080 --rate 144"
       "xrandr --output DisplayPort-1 --mode 1920x1080 --rate 144"
