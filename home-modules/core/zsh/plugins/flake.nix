2967c9ad355d2a505333bfeed27270c4b5c95a48
Reed <mrnitsuj1@gmail.com>
2026-02-28 10:43:47 -0500

    home-modules(core/zsh/plugins): fix direnv-instant prompt redraw

--- home-modules/core/zsh/plugins/flake.nix
+++ home-modules/core/zsh/plugins/flake.nix
@@ -5,7 +5,8 @@
       flake = false;
     };
     direnv-instant = {
-      url = "github:Mic92/direnv-instant";
+      # https://github.com/Mic92/direnv-instant/pull/68
+      url = "github:XYenon/direnv-instant/86fd7cbb4a6d893a87090162fe9e875ae72e6da4";
     };
   };
   outputs = _: { };
