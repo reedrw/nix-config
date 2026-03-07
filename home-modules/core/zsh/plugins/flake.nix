dfa1e20071272bd8b199bae65ade16008e687031
Reed <mrnitsuj1@gmail.com>
2026-03-06 20:18:43 -0500

    home(modules/core/zsh): unpin direnv-instant

--- home-modules/core/zsh/plugins/flake.nix
+++ home-modules/core/zsh/plugins/flake.nix
@@ -4,10 +4,7 @@
       url = "github:DeveloperC286/zsh-simple-abbreviations";
       flake = false;
     };
-    direnv-instant = {
-      # https://github.com/Mic92/direnv-instant/pull/68
-      url = "github:XYenon/direnv-instant/86fd7cbb4a6d893a87090162fe9e875ae72e6da4";
-    };
+    direnv-instant.url = "github:Mic92/direnv-instant";
   };
   outputs = _: { };
 }
