38586f9dfcf89b2746348a24b610f59525bcd7e1
Reed <mrnitsuj1@gmail.com>
2026-03-30 10:16:23 -0400

    home-modules(games/3ds): install azahar

--- home-modules/games/3ds/default.nix
+++ home-modules/games/3ds/default.nix
@@ -3,11 +3,18 @@ let
   tlaterpkgs = (util.importFlake ./sources).inputs.tlaterpkgs;
 in
 {
-  home.packages = (with pkgs.nur.repos.ihaveamac; [
+  home.packages = (with pkgs; [
+    azahar # Arctic Base
+  ]) ++ (with pkgs.nur.repos.ihaveamac; [
     ctrtool
     cxitool
     makerom
   ]) ++ (with tlaterpkgs.packages."${pkgs.stdenv.hostPlatform.system}"; [
     servefiles
   ]);
+
+  custom.persistence.directories = [
+    ".config/azahar-emu"
+    ".local/share/azahar-emu"
+  ];
 }
