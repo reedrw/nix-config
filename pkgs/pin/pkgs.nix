f25ac58410d4e73d0950e2f660a4e60e736360db
Reed <mrnitsuj1@gmail.com>
2026-04-09 17:38:45 -0400

    pkgs(pin): pin updog at 2.0.1; use replaceStdenv

--- pkgs/pin/pkgs.nix
+++ pkgs/pin/pkgs.nix
@@ -1,13 +1,21 @@
 pkgs: let
   lib = pkgs.lib;
   json = lib.importJSON ./pinned.json;
-  pinnedVersionToPkg = { rev, sha256, ... }: import (builtins.fetchTarball {
+  pinnedVersionToPkg = { rev, sha256, ... }: lib.pipe (import (builtins.fetchTarball {
     inherit sha256;
     url = "https://github.com/nixos/nixpkgs/archive/${rev}.tar.gz";
   }) {
-    inherit (pkgs) config;
     inherit (pkgs.stdenv.hostPlatform) system;
-  };
+  }) [
+    # `config` backwards incompatible after
+    # https://github.com/NixOS/nixpkgs/commit/8a5aae22c02b8d0f8306abb1bed314c569c3700f
+    (prev: import "${prev.path}" {
+      config = pkgs.config // {
+        replaceStdenv = _: prev.stdenv;
+      };
+      inherit (prev.stdenv.hostPlatform) system;
+    })
+  ];
 
   mapToPackage = package: a: lib.mapAttrs' (n: v: let
     pinnedPkgs =
