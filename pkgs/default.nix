0f7e3408359177a9eb94620731a78917d38418e9
Reed <mrnitsuj1@gmail.com>
2026-06-14 10:00:26 -0400

    pkgs(fluxer): drop

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,7 +3,6 @@ let
   myPkgs = {
     gc = pkgs.callPackage ./gc { };
     jdownloader = pkgs.callPackage ./jdownloader { };
-    fluxer = pkgs.callPackage ./fluxer { };
     ldp = self.callPackage ./ldp { };
     mountiso = pkgs.callPackage ./mountiso { };
     pin = pkgs.callPackage ./pin { };
