c948871f233c0a7c4101031eb73204cde7990b7a
Reed <mrnitsuj1@gmail.com>
2026-06-22 08:13:01 -0400

    home-configurations(reed@nixos-desktop/sway): remove unneeded outputs

--- home-configurations/reed@nixos-desktop/sway.nix
+++ home-configurations/reed@nixos-desktop/sway.nix
@@ -1,9 +1,5 @@
 {
   wayland.windowManager.sway.config.output = {
     "DP-1" = { mode = "2650x1440@144Hz"; };
-    "DP-2" = { mode = "2650x1440@144Hz"; };
-    "DP-3" = { mode = "2650x1440@144Hz"; };
-    "DP-4" = { mode = "2650x1440@144Hz"; };
-    "DP-5" = { mode = "2650x1440@144Hz"; };
   };
 }
