58f165c7a9a02ddd63dd9d8f64311de10226fade
Reed <mrnitsuj1@gmail.com>
2026-06-22 08:13:35 -0400

    home-configurations(reed@nixos-desktop/sway): add `brightness` alias

--- home-configurations/reed@nixos-desktop/sway.nix
+++ home-configurations/reed@nixos-desktop/sway.nix
@@ -1,5 +1,14 @@
+{ pkgs, ... }:
+
 {
   wayland.windowManager.sway.config.output = {
     "DP-1" = { mode = "2650x1440@144Hz"; };
   };
+
+  home.packages = with pkgs; [
+    ddcutil
+    (pkgs.aliasToPackage {
+      brightness = ''ddcutil setvcp 10 "$@"'';
+    })
+  ];
 }
