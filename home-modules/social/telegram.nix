2f8981156a54d12ade703a2ab50247d7e6bf6cbe
Reed <mrnitsuj1@gmail.com>
2026-07-01 10:57:49 -0400

    home-modules(social/telegram): remove button-layout workaround

--- home-modules/social/telegram.nix
+++ home-modules/social/telegram.nix
@@ -13,12 +13,6 @@
     })
   ];
 
-  # doesn't do anything on i3, but needed for Telegram to close without minimizing
-  # https://github.com/telegramdesktop/tdesktop/issues/27190#issuecomment-1840780300
-  dconf.settings = {
-    "org/gnome/desktop/wm/preferences".button-layout = ":minimize,maximize,close";
-  };
-
   custom.persistence.directories = [
     ".local/share/TelegramDesktop"
   ];
