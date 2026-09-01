ce0f8bcfa8ad017f43e1aabc73b03430f047f1c0
Reed <mrnitsuj1@gmail.com>
2026-09-01 18:13:07 -0400

    home-modules(filesharing/torrent): hard-code qbittorrent port

--- home-modules/filesharing/torrent.nix
+++ home-modules/filesharing/torrent.nix
@@ -2,10 +2,9 @@
 
 {
   home.packages = [ pkgs.mktorrent ];
-  systemd.user.services = config.lib.functions.mkSimpleService "qbittorrent"
-    <| lib.getExe
-    <| pkgs.mullvadExclude
-    <| pkgs.qbittorrent-nox;
+  systemd.user.services = config.lib.functions.mkSimpleService "qbittorrent" ''
+    ${lib.getExe (pkgs.mullvadExclude pkgs.qbittorrent-nox)} --webui-port=8181
+  '';
 
   custom.persistence.directories = [
     ".config/qBittorrent"
